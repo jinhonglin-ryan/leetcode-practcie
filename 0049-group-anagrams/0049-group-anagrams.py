722eb80008a9b184b151bda151757b2d795b4bce
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-17 22:53:53 -0400

    Time: 62 ms (93.81%), Space: 16.3 MB (35.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -5,18 +5,34 @@ class Solution(object):
         :rtype: List[List[str]]
         """
         
-        sorted_str_to_index = {}
+#         sorted_str_to_index = {}
+#         for i, s in enumerate(strs):
+#             s_sorted = str(sorted(s))
+#             if s_sorted in sorted_str_to_index:
+#                 sorted_str_to_index[s_sorted].append(i)
+#             else: 
+#                 sorted_str_to_index[s_sorted] = [i]
+        
+#         res = defaultdict(list)
+        
+#         for anagrams, indices in sorted_str_to_index.items():
+#             for idx in indices:
+#                 res[anagrams].append(strs[idx])
+        
+#         return list(res.values()) 
+        
+        sorted_str_to_index = defaultdict(list)
+
+        # 填充字典
         for i, s in enumerate(strs):
-            s_sorted = str(sorted(s))
-            if s_sorted in sorted_str_to_index:
-                sorted_str_to_index[s_sorted].append(i)
-            else: 
-                sorted_str_to_index[s_sorted] = [i]
-        
-        res = defaultdict(list)
-        
-        for anagrams, indices in sorted_str_to_index.items():
-            for idx in indices:
-                res[anagrams].append(strs[idx])
-        
-        return list(res.values()) 
+            s_sorted = tuple(sorted(s))  # 使用元组作为键
+            sorted_str_to_index[s_sorted].append(s)  # 直接存储字符串
+
+        # 初始化结果列表
+        res = []
+
+        # 遍历字典并构建结果
+        for anagrams in sorted_str_to_index.values():
+            res.append(anagrams)
+
+        return res
