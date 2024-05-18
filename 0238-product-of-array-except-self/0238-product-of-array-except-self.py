4bc824bbf0d2436d56b9c1d8a6726bb15bc59426
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-18 00:27:24 -0400

    Time: 292 ms (38.73%), Space: 21.7 MB (20.85%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,9 +5,14 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        res = [1] * len(nums)
-        left = [1] * len(nums)
+        
+        # i-th element is the product of elements from index 0 to i - 1
+        left = [1] * len(nums) 
+        # i-th element is the product of elements from len - 1 to i + 1
         right = [1] * len(nums)
+        # res[i] = left[i] * right[i]
+        res = [1] * len(nums)
+        
         
         for i in range(1, len(nums)):
             left[i] = left[i - 1] * nums[i - 1]
