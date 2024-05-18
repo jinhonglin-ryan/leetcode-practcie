54bca2202b628671cf0966d064bce9c0163ee24f
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-17 23:59:26 -0400

    Time: 20 ms (32.56%), Space: 11.9 MB (64.73%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -5,13 +5,26 @@ class Solution(object):
         :rtype: str
         """
         
-        length = len(strs[0])
-        count = len(strs)
+        # 以第一个str作为基准
+#         length = len(strs[0])
+#         count = len(strs)
         
-        for i in range(length):
-            c = strs[0][i]
-            for j in range(1, count):
-                if (len(strs[j]) == i) or (strs[j][i] != c):
-                    return strs[0][:i]
-        return strs[0]
+#         for i in range(length):
+#             c = strs[0][i]
+            
+#             for j in range(1, count):
+#                 if (len(strs[j]) == i) or (strs[j][i] != c):
+#                     return strs[0][:i]
+#         return strs[0]
+        prefix = strs[0]
+
+        # 依次比较每个字符串
+        for s in strs[1:]:
+            # 缩短前缀直到找到匹配的前缀
+            while s.find(prefix) != 0:
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+
+        return prefix
         
