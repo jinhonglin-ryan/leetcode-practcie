bce2429f2817006854b56000b063e5b2c81e50dd
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-17 20:56:14 -0400

    Time: 427 ms (63.90%), Space: 27.6 MB (24.55%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -5,11 +5,29 @@ class Solution(object):
         :rtype: bool
         """
 
+        # Method 1: Use Set() to store duplicate elements
         seen = set()
         for num in nums:
             if num in seen:
                 return True
             seen.add(num)
         return False
+        
+
+        # Method 2: Use Dictionary (each element is mapped to a count) 
+        # to store duplicate elements 
+
+        # count = {}
+
+        # for num in nums:
+        #     if num in count:
+        #         count[num] += 1
+        #     else:
+        #         count[num] = 1
+        
+        # for key in count:
+        #     if count[key] > 1:
+        #         return True
+        # return False
 
 
