975ce5efc9b3eada5a9fe83a7589975253a2a933
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-17 21:05:43 -0400

    Time: 44 ms (17.19%), Space: 11.9 MB (98.52%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -14,15 +14,24 @@ class Solution(object):
     
         
         # Method 2: use Dictionary to store count of each character
-        if len(s) != len(t):
-            return False
+#         if len(s) != len(t):
+#             return False
         
-        s_count = {}
-        t_count = {}
+#         # dictionary: maps character to its count
+#         s_count = {}
+#         t_count = {}
         
-        for char in s:
-            s_count[char] = s_count.get(char, 0) + 1
-        for char in t:
-            t_count[char] = t_count.get(char, 0) + 1
+#         # iterate over character
+#         # dict.get(char, 0) means:
+#         # 1. try to get the value that key "char" points to
+#         # 2. if there is no such key "char" existed, return 0 
         
-        return s_count == t_count
+#         for char in s:
+#             s_count[char] = s_count.get(char, 0) + 1
+#         for char in t:
+#             t_count[char] = t_count.get(char, 0) + 1
+        
+#         return s_count == t_count
+
+        # Method 3: use Counter()
+        return Counter(s) == Counter(t)
