c076a392267dd5d5ece7a53005eb8a7402e709d9
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-17 21:03:27 -0400

    Time: 37 ms (42.50%), Space: 12 MB (80.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -6,6 +6,23 @@ class Solution(object):
         :rtype: bool
         """
         
-        if sorted(s) == sorted(t):
-            return True
-        return False
+        # Method 1: use sorted()
+        # if two strings after sorted are equal, they are anagrams
+        # if sorted(s) == sorted(t):
+        #     return True
+        # return False
+    
+        
+        # Method 2: use Dictionary to store count of each character
+        if len(s) != len(t):
+            return False
+        
+        s_count = {}
+        t_count = {}
+        
+        for char in s:
+            s_count[char] = s_count.get(char, 0) + 1
+        for char in t:
+            t_count[char] = t_count.get(char, 0) + 1
+        
+        return s_count == t_count
