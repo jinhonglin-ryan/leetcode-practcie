4995facb73023e80b5ecb388e72d4e35dca30dd2
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-17 23:00:28 -0400

    Time: 86 ms (21.76%), Space: 15.9 MB (57.01%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -5,11 +5,14 @@ class Solution(object):
         :rtype: List[List[str]]
         """
         
+        # anagram str -> a list of anagrams strings
         sorted_str_to_index = {}
         res = []
-        for s in strs:
+        for s in strs: 
+            # use sorted(s) as the key 
             s_sorted = str(sorted(s))
             if s_sorted in sorted_str_to_index:
+                # a list += a list is still a list with s appended at the end
                 sorted_str_to_index[s_sorted] += [s]
             else:
                 sorted_str_to_index[s_sorted] = [s]
