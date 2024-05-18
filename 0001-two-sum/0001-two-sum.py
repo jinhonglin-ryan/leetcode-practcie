c86d957c9f4e354cffcf555982a4270754383e48
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-17 21:19:38 -0400

    Time: 32 ms (92.67%), Space: 12.5 MB (77.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,7 +6,27 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if (nums[i] + nums[j]) == target:
-                    return [i, j]
+        # Method 1: 暴力解法
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if (nums[i] + nums[j]) == target:
+        #             return [i, j]
+                
+        # Method 2: 
+        num_to_index = {}
+
+    # 遍历数组
+        for i, num in enumerate(nums):
+            # 计算所需的补数
+            complement = target - num
+
+            # 检查补数是否已经在字典中
+            if complement in num_to_index:
+                # 如果在字典中，返回补数的索引和当前数字的索引
+                return [num_to_index[complement], i]
+
+            # 如果不在字典中，将当前数字和其索引加入字典
+            num_to_index[num] = i
+
+        # 如果没有找到符合条件的两个数字，返回一个空列表
+        return []
