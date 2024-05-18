16898de9e69d614c8ef9d010163bc4abe41523b1
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-18 00:03:35 -0400

    Time: 28 ms (6.27%), Space: 11.7 MB (89.66%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -6,25 +6,21 @@ class Solution(object):
         """
         
         # 以第一个str作为基准
-#         length = len(strs[0])
-#         count = len(strs)
+        length = len(strs[0])
+        count = len(strs)
         
-#         for i in range(length):
-#             c = strs[0][i]
+        # 遍历第一个string by character
+        for i in range(length):
+            c = strs[0][i]
             
-#             for j in range(1, count):
-#                 if (len(strs[j]) == i) or (strs[j][i] != c):
-#                     return strs[0][:i]
-#         return strs[0]
-        prefix = strs[0]
-
-        # 依次比较每个字符串
-        for s in strs[1:]:
-            # 缩短前缀直到找到匹配的前缀
-            while s.find(prefix) != 0:
-                prefix = prefix[:-1]
-                if not prefix:
-                    return ""
-
-        return prefix
+            # 对于第i-th character，遍历strs中的其他string，比较在i这个位置的character
+            # 是否和c一样
+            for j in range(1, count):
+                # 如果当前string的长度就是i的长度，遍历结束
+                # 或者 如果当前string第i个位置的character 跟c不一样 遍历结束
+                if (len(strs[j]) == i) or (strs[j][i] != c):
+                    # 返回第一个string的前i个character作为longest common prefix
+                    return strs[0][:i]
+        # code执行到这 说明一直没有退出，则整个first string是longest common prefix
+        return strs[0]
         
