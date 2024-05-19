fcd8cca254da176931660a02fa5f2a8d5ee14971
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-19 01:42:00 -0400

    Time: 1075 ms (43.66%), Space: 15.1 MB (59.61%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,12 +5,12 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         
-        # 固定一个数字，然后用two-pointer来找有没有sum to 0
+        # 固定一个数字，然后用two-pointer从下一个位置到结尾来找有没有sum to 0
         
         sorted_num = sorted(nums)
         res = []
         for i in range(len(nums)):
-            # if we have already had included all the possible result for that number indexed at i (or i - 1), so skip
+            # we have already had included all the possible result for that number indexed at i (or i - 1), so skip
             if i > 0 and sorted_num[i] == sorted_num[i - 1]:
                 continue
             
@@ -43,7 +43,10 @@ class Solution(object):
                         left += 1
                     while left < right and sorted_num[right] == sorted_num[right - 1]:
                         right -= 1
-                        
+                    # 这两句保证我们走到下一个不同的数字上
+                    # 比如在一次left += 1后，sorted_num[left] != sorted_num[left + 1]
+                    # 这个循环退出，我们知道left + 1 能给我们一个新的数字，所以需要left += 1
+                    # 走到那个位置
                     left += 1
                     right -= 1
                 
