68fb1d73249d0a661470f9afc766cfd6f165bfcc
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-19 00:45:42 -0400

    Time: 5333 ms (7.25%), Space: 29 MB (27.48%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -11,12 +11,17 @@ class Solution(object):
         longest_length = 0
         
         for num in nums:
+            # if num - 1 is in the set, 
+            # then the current num is not the start, skip this iteration
             if (num - 1) in num_set:
                 continue
-            # assume this is the starting of the sequence
-            if num - 1 not in num_set:
+                
+            # if num - 1 is not in the set
+            # then the current num is the start of the sequence
+            if (num - 1) not in num_set:
                 current_num = num
                 current_length = 1
+                
                 
                 while current_num + 1 in num_set:
                     current_length += 1
