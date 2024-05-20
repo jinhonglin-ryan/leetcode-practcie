5a2a4fbefe428f4f9a86f4994a57e4c443132ebd
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-20 03:10:14 -0400

    Time: 183 ms (81.86%), Space: 22.3 MB (40.93%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -6,21 +6,29 @@ class Solution(object):
         :rtype: int
         """
         
-        left = 0
+        """
+        滑动窗口
+        """
+        left = 0 
         right = 0
-        n = len(nums)
         curr_sum = 0
-        min_len = n + 1
+        min_len = len(nums) + 1
         
-        while right < n:
+        while right < len(nums):
             curr_sum += nums[right]
             
+            # 只要curr_sum >= target, 说明满足题目条件，我们要更新min_len
+            # 然后减去left的值，并且右移left更新窗口
+            # 直到curr_sum < target，我们右移right, 加入一个新的值到窗口中
             while curr_sum >= target:
                 min_len = min(min_len, right - left + 1)
                 curr_sum -= nums[left]
                 left += 1
+            
             right += 1
-        return min_len if min_len != n + 1 else 0 
+            
+        # 如果没有这个array返回0
+        return min_len if min_len != len(nums) + 1 else 0 
         
         
         
