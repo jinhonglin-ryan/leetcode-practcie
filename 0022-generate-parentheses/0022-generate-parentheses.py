6112b868cbfb39b450193cdf43d3c36667f3e0b4
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-22 00:20:14 -0400

    Time: 17 ms (55.80%), Space: 12.2 MB (11.78%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -5,6 +5,7 @@ class Solution(object):
         :rtype: List[str]
         """
         
+        # Method 1: 
 #         all_combo = []
 #         curr_combo = []
         
@@ -24,17 +25,35 @@ class Solution(object):
 #         backtrack(0, 0)
 #         return all_combo
     
-    
-        res = []
+        # Method 2: 
+#         res = []
+#         # left: # of ( available to use
+#         # right: # of ) available to use
+#         # curr_string: current combination of ( and )
         
-        def dfs(left, right, curr_string):
-            if left == 0 and right == 0:
-                res.append(curr_string)
-                return 
-            if left > 0:
-                dfs(left - 1, right, curr_string + "(")
-            if right > left:
-                dfs(left, right - 1, curr_string + ")")
+#         def dfs(left, right, curr_string):
+#             # print(curr_string)
+#             if left == 0 and right == 0:
+#                 res.append(curr_string)
+#                 return 
+#             if left > 0:
+#                 dfs(left - 1, right, curr_string + "(")
+#             if right > left:
+#                 dfs(left, right - 1, curr_string + ")")
                 
-        dfs(n, n, "")
-        return res 
+#         dfs(n, n, "")
+#         return res 
+
+        # Method 3:
+        ans = []
+        
+        def dfs(lc, rc, n, curr_string):
+            if lc == n and rc == n:
+                ans.append(curr_string)
+            else:
+                if lc < n:
+                    dfs(lc + 1, rc, n, curr_string + "(")
+                if rc < n and lc > rc:
+                    dfs(lc, rc + 1, n, curr_string + ")")
+        dfs(0, 0, n, "")       
+        return ans
