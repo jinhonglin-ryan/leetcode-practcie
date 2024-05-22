e2c6f1a9e30ab25a5614b30e61948a1d81a64910
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-22 19:01:30 -0400

    Time: 19 ms (41.82%), Space: 11.9 MB (70.17%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -5,28 +5,28 @@ class Solution(object):
         :rtype: List[str]
         """
         
-#         ans = []
-#         def dfs(lc, rc, n, curr):
-#             if lc == 0 and rc == 0:
-#                 ans.append(curr)
-#             else: 
-#                 if lc < n:
-#                     dfs(lc + 1, rc, n, curr + "(")
-#                 if rc < n and lc > rc:
-#                     dfs(lc, rc + 1, n, curr + ")")
-        
-#         dfs(0, 0, n, "")
-#         return ans 
-                
         ans = []
-        
-        def dfs(lc, rc, n, curr_string):
+        def dfs(lc, rc, curr):
             if lc == n and rc == n:
-                ans.append(curr_string)
-            else:
+                ans.append(curr)
+            else: 
                 if lc < n:
-                    dfs(lc + 1, rc, n, curr_string + "(")
+                    dfs(lc + 1, rc, curr + "(")
                 if rc < n and lc > rc:
-                    dfs(lc, rc + 1, n, curr_string + ")")
-        dfs(0, 0, n, "")       
-        return ans
+                    dfs(lc, rc + 1, curr + ")")
+        
+        dfs(0, 0, "")
+        return ans 
+                
+#         ans = []
+        
+#         def dfs(lc, rc, n, curr_string):
+#             if lc == n and rc == n:
+#                 ans.append(curr_string)
+#             else:
+#                 if lc < n:
+#                     dfs(lc + 1, rc, n, curr_string + "(")
+#                 if rc < n and lc > rc:
+#                     dfs(lc, rc + 1, n, curr_string + ")")
+#         dfs(0, 0, n, "")       
+#         return ans
