28964c7c638e2bc3bae2d3ea19c5d044a7eb8dd7
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-21 23:04:35 -0400

    Time: 13 ms (82.23%), Space: 11.8 MB (70.25%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -5,21 +5,36 @@ class Solution(object):
         :rtype: List[str]
         """
         
-        all_combo = []
-        curr_combo = []
+#         all_combo = []
+#         curr_combo = []
         
-        def backtrack(symbol, index):
-            if 2 * n == index:
-                if symbol == 0:
-                    all_combo.append("".join(curr_combo))
-            else:
-                if symbol < n:
-                    curr_combo.append('(')
-                    backtrack(symbol + 1, index + 1)
-                    curr_combo.pop()
-                if symbol > 0: 
-                    curr_combo.append(')')
-                    backtrack(symbol - 1, index + 1)
-                    curr_combo.pop()
-        backtrack(0, 0)
-        return all_combo
+#         def backtrack(symbol, index):
+#             if 2 * n == index:
+#                 if symbol == 0:
+#                     all_combo.append("".join(curr_combo))
+#             else:
+#                 if symbol < n:
+#                     curr_combo.append('(')
+#                     backtrack(symbol + 1, index + 1)
+#                     curr_combo.pop()
+#                 if symbol > 0: 
+#                     curr_combo.append(')')
+#                     backtrack(symbol - 1, index + 1)
+#                     curr_combo.pop()
+#         backtrack(0, 0)
+#         return all_combo
+    
+    
+        res = []
+        
+        def dfs(left, right, curr_string):
+            if left == 0 and right == 0:
+                res.append(curr_string)
+                return 
+            if left > 0:
+                dfs(left - 1, right, curr_string + "(")
+            if right > left:
+                dfs(left, right - 1, curr_string + ")")
+                
+        dfs(n, n, "")
+        return res 
