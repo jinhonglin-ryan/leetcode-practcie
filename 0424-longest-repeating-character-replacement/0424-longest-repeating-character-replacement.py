f96408b8a922743e01399d8cf7d503c41a99571c
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-21 21:57:55 -0400

    Time: 155 ms (10.05%), Space: 11.9 MB (84.92%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -6,23 +6,39 @@ class Solution(object):
         :rtype: int
         """
         
-        max_len = -1
+        """
+        滑动窗口
+        * 核心思想：
+        1.
+            如果当前窗口的间距 > 当前出现最大次数的字符的次数 + k 时
+            意味着替换 k 次仍不能使当前窗口中的字符全变为相同字符，
+            则此时应该将左边界右移，同时将原先左边界的字符频次减少。
+        2.  
+            可以考虑 s=DBCAB, k=2, 最好应该是BBBB, output为4
+            go through code to see how max len is updated
+        """
         
-        counter = Counter()
-        right = 0
+        max_len = -1
         left = 0
+        right = 0
+        
+        window = Counter(s)
+        curr_window = Counter()
         
         while right < len(s):
-            counter[s[right]] += 1
+            # update当前right指针指向的character的frequency
+            curr_window[s[right]] += 1
             
-            while right - left + 1 > max(counter.values()) + k:
-                counter[s[left]] -= 1
+            # 当当前窗口间距 > 当前出现最大次数的字符的次数 + k
+            # 替换 k 次不能使当前窗口中的字符全变为相同字符
+            while right - left + 1 > max(curr_window.values()) + k:
+                curr_window[s[left]] -= 1
                 left += 1
-            
+                
+            # 当当前窗口间距 <= 当前出现最大次数的字符的次数 + k时
+            # 替换 k 次可以使得当前窗口中的字符全变为相同字符
+            # 所以在这维护max_len
             max_len = max(max_len, right - left + 1)
-            
-            
             right += 1
-        
-        return max_len
-        
+        return max_len 
+            
