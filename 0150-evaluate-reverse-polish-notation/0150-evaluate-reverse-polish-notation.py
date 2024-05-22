343f8e25e35b3a0d2aa9f83dc2f3db3b1d0ed05d
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-22 18:53:04 -0400

    Time: 41 ms (71.94%), Space: 13.7 MB (21.09%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -6,29 +6,21 @@ class Solution(object):
         """
         
         stack = []
-
+        
         for token in tokens:
             if token in "+-*/":
                 num_1 = stack.pop()
                 num_2 = stack.pop()
-
+                
                 if token == '+':
-                    result = num_2 + num_1
+                    stack.append(num_1 + num_2)
                 elif token == '-':
-                    result = num_2 - num_1
+                    stack.append(num_2 - num_1)
                 elif token == '*':
-                    result = num_2 * num_1
-                elif token == '/':
-                    # 使用 // 运算符实现向零截断
-                    # 向0截断意思：
-                    # -5 / 2 需要返回 -2，向0靠近
-                    # 如果直接 int(num_2 / num_1)的话，-5 / 2 返回的是 -3，向负无穷取floor
-                    # 因此我们需要
-                    result = int(num_2 / num_1) if num_2 * num_1 >= 0 else -(abs(num_2) // abs(num_1))
-
-                stack.append(result)
+                    stack.append(num_2 * num_1)
+                elif token == "/":
+                    result = int(num_2 / num_1) if num_2 * num_1 > 0 else -(-num_2 // num_1)
+                    stack.append(result)
             else:
-                # 操作数压入栈中
                 stack.append(int(token))
-
         return stack.pop()
