d284843314f1fff124ebc2d4329cb24f7d5d4ac6
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-23 02:45:04 -0400

    Time: 23 ms (20.18%), Space: 13.4 MB (93.78%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -10,15 +10,16 @@ class Solution(object):
         :rtype: ListNode
         """
         
+        prev = None
+        curr = head
         
-        pre = None
-        cur = head
-        while cur != None:
-            next = cur.next
-            cur.next = pre
-            pre = cur
-            cur = next
-        return pre
+        while curr is not None:
+            next_tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_tmp
+        return prev 
+        
         
          
         
