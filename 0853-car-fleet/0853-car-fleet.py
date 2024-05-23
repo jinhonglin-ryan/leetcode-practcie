7a17f3abe60120e1e558e0c4fbfbb76816920fca
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-22 21:06:56 -0400

    Time: 759 ms (69.61%), Space: 30 MB (99.02%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        """
+        :type target: int
+        :type position: List[int]
+        :type speed: List[int]
+        :rtype: int
+        """
+        n = len(position)
+        id = list(range(n))
+        id.sort(key=lambda x: position[x])
+        
+        res = n
+        eps = 1e-6
+        last = 0.0
+        for i in range(n - 1, -1, -1):
+            t = (target - position[id[i]]) / float(speed[id[i]])
+            if t < last + eps:
+                res -= 1
+            else:
+                last = t
+        
+        return res
+        
