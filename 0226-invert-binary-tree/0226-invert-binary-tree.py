36738e5296dea1bac6e5bf1fb7248428fab5d3ff
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-24 04:44:27 -0400

    Time: 18 ms (23.25%), Space: 11.7 MB (22.19%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -17,22 +17,23 @@ class Solution(object):
             return None
         
         # 可以先交换左右子树，再分别翻转左右子树
-#         temp = root.left
-#         root.left = root.right
-#         root.right = temp
+        temp = root.left
+        root.left = root.right
+        root.right = temp
         
         
-#         self.invertTree(root.left)
-#         self.invertTree(root.right)
-#         return root 
+       
+        self.invertTree(root.right)
+        self.invertTree(root.left)
+        return root 
     
     
         # 或者先翻转左右子树，再交换左右子树
-        left = self.invertTree(root.left)
-        right = self.invertTree(root.right)
-        root.right = left 
-        root.left = right
-        return root 
+        # left = self.invertTree(root.left)
+        # right = self.invertTree(root.right)
+        # root.right = left 
+        # root.left = right
+        # return root 
         
         
         
