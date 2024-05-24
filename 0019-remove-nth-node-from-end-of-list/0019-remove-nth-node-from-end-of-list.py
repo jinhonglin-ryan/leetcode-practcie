dbf4f320c860596f7583b6709f6052eea537994f
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-24 04:21:28 -0400

    Time: 16 ms (54.74%), Space: 11.5 MB (79.79%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -11,34 +11,61 @@ class Solution(object):
         :rtype: ListNode
         """
         
+#         # Method 1: 屎山代码 本人原创
+#         k = 0 
+#         curr = head
+#         while curr is not None:
+#             k += 1
+#             curr = curr.next
+        
+#         if k == 1 and n == 1:
+#             head = None
+#             return head
+        
+#         # k is the length of the linked list
+#         if k == n:
+#             return head.next 
+        
+#         curr = head
+#         while k > n + 1 and curr is not None:
+#             curr = curr.next
+#             k -= 1
+            
+#         if curr is None:
+#             return head
+        
+#         if curr.next is None:
+#             return head
+        
+#         if curr.next.next is None:
+#             curr.next = None
+#             return head
+        
+#         tmp = curr.next.next
+#         curr.next = tmp
+#         return head
+    
+        # Method 2：yxc 方法，类似，但是更精简
         k = 0 
+        dummy = ListNode()
+        dummy.next = head
         curr = head
         while curr is not None:
             k += 1
             curr = curr.next
         
-        if k == 1 and n == 1:
-            head = None
-            return head
+        # 要删除倒数第n个点，我们需要找到倒数第n+1个点
+        # 意思就是从第一个数开始，需要移动k - n - 1步 才能到倒数n+1个点
+        # 找到倒数第n+1个点之后，删除第n个点就很方便了
+        # k = 5, n = 2
+        index = 0
+        temp = dummy 
+        for index in range(0, k - n):
+            temp = temp.next
+        temp.next = temp.next.next
         
-        # k is the length of the linked list k = 2 n = 2
-        if k == n:
-            return head.next 
+        return dummy.next 
+            
         
-        curr = head
-        while k > n + 1 and curr is not None:
-            curr = curr.next
-            k -= 1
-        if curr is None:
-            return head
         
-        if curr.next is None:
-            return head
         
-        if curr.next.next is None:
-            curr.next = None
-            return head
-        
-        tmp = curr.next.next
-        curr.next = tmp
-        return head
