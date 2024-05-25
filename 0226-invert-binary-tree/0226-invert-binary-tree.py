bd0b2444cd9ec1bcdb22d409c35629f5ba837352
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-25 14:59:59 -0400

    Time: 19 ms (17.68%), Space: 11.6 MB (85.70%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -11,29 +11,13 @@ class Solution(object):
         :rtype: TreeNode
         """
         
-        # recursion 
+        if root is None:
+            return
         
-        if not root:
-            return None
+        left = self.invertTree(root.left)
+        right = self.invertTree(root.right)
         
-        # 可以先交换左右子树，再分别翻转左右子树
-        temp = root.left
-        root.left = root.right
-        root.right = temp
+        root.right = left 
+        root.left = right
         
-        
-       
-        self.invertTree(root.right)
-        self.invertTree(root.left)
         return root 
-    
-    
-        # 或者先翻转左右子树，再交换左右子树
-        # left = self.invertTree(root.left)
-        # right = self.invertTree(root.right)
-        # root.right = left 
-        # root.left = right
-        # return root 
-        
-        
-        
