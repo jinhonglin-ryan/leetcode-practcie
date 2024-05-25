c853c4021cb1e74b1c21200ba033ea2d71c5bab4
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-25 14:54:33 -0400

    Time: 38 ms (33.20%), Space: 19 MB (19.18%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -11,62 +11,13 @@ class Solution(object):
         :rtype: bool
         """
         
-        # Method 1: set()
+        all_set = set()
         
-#         node_set = set()
-#         curr = head
+        curr = head 
         
-#         while curr:
-#             if curr in node_set:
-#                 return True
-#             node_set.add(curr)
-#             curr = curr.next
-            
-#         return False 
-    
-        
-        # Method 2: 快慢双指针
-        # 如果list为null或者只有一个node，肯定没有环，return false 
-        if not head or not head.next:
-            return False
-        # 设置快慢双指针，初始化将两个指针错开
-        # 快指针每次走两步，慢指针每次走一步
-        # 因此如果有环，快指针和慢指针肯定最终会相遇，因为相对速度为1，有环的话，每次迭代，快慢指针之间的距离缩减1
-        # 如果没有环的话，快指针会先走到null，那么就return false 表示没有环
-        slow = head
-        fast = head.next
-        
-        # 写法1：
-        # 在while里面check if not fast or not fast.next:
-        # 这是为了保证fast = fast.next.next 不会报错（不会是None.next.next or None.next），如果fast 或者fast.next是null了，说明没有环。return False
-#         while slow != fast:
-#             if not fast or not fast.next:
-#                 return False
-#             slow = slow.next
-#             fast = fast.next.next
-        
-#         # 如果slow == fast 直接返回true
-#         return True
-        
-        # 写法2：
-        
-        while fast:
-            slow = slow.next
-            fast = fast.next
-            if not fast:
-                return False
-            fast = fast.next
-            if slow == fast:
+        while curr:
+            if curr in all_set:
                 return True
-        return False
-        # Method 3: 哈希表
-#         node_to_count = {}
-        
-#         curr = head
-#         while curr:
-#             if curr in node_to_count:
-#                 return True
-#             else:
-#                 node_to_count[curr] = 1
-#             curr = curr.next
-#         return False 
+            all_set.add(curr)
+            curr = curr.next
+        return False 
