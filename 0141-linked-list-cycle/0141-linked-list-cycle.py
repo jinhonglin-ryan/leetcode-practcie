738c333b4d6c0cfb905a386a6abbb7c641ed63cd
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-24 21:44:33 -0400

    Time: 25 ms (94.25%), Space: 18.6 MB (89.87%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -11,15 +11,31 @@ class Solution(object):
         :rtype: bool
         """
         
-        # Method 1 set()
+        # Method 1: set()
         
-        node_set = set()
-        curr = head
+#         node_set = set()
+#         curr = head
         
-        while curr:
-            if curr in node_set:
-                return True
-            node_set.add(curr)
-            curr = curr.next
+#         while curr:
+#             if curr in node_set:
+#                 return True
+#             node_set.add(curr)
+#             curr = curr.next
             
-        return False 
+#         return False 
+    
+        
+        # Method 2: 快慢双指针
+        if not head or not head.next:
+            return False
+        
+        slow = head
+        fast = head.next
+        
+        while slow != fast:
+            if not fast or not fast.next:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        
+        return True
