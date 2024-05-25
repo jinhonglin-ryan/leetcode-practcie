a802595c3253104b7880546381783a5a33396de4
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-25 00:18:15 -0400

    Time: 625 ms (6.49%), Space: 23.3 MB (88.02%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -5,9 +5,35 @@ class Solution(object):
         :rtype: int
         """
         
-        count = {}
-        for num in nums:
-            if num in count:
-                return num
-            count[num] = 1
-        return 
+        
+        # Method 1: 不满足要求，空间复杂度是O(n)
+        # count = {}
+        # for num in nums:
+        #     if num in count:
+        #         return num
+        #     count[num] = 1
+        # return -1 
+    
+    
+        # Method 2: 二分
+        # [left, right] 初始化表示 [1, n] 这个区间
+        # 取这个区间的中间值 n + 1 // 2
+        left = 1
+        right = len(nums) - 1 # n + 1 - 1 = n
+        
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            
+            count = 0
+            
+            for num in nums:
+                if num <= mid:
+                    count += 1
+                    
+            if count <= mid:
+                left = mid + 1 
+            else:
+                right = mid
+        return left 
+        
