e43175d95b0ef89defb545c50e4dab564e63efe6
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-25 00:38:23 -0400

    Time: 642 ms (5.09%), Space: 23.6 MB (47.90%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -17,7 +17,17 @@ class Solution(object):
     
         # Method 2: 二分
         # [left, right] 初始化表示 [1, n] 这个区间
-        # 取这个区间的中间值 n + 1 // 2
+        # 取这个区间的中间值
+        # 比如我们的n是5，[1, 2, 3, 4, 5] 中间值就是3
+        # 我们将[1, n]这个区间分成了[left, mid] 和 [mid + 1, right] 
+        # 一开始的时候 left = 1, right = n
+        # 然后遍历nums， 记录小于等于中间值的count
+        # 在一个没有重复的数组中，[1, mid] 中小于等于mid的数应该有mid个
+        # 但是如果有重复的情况的话，就需要分类讨论
+        # 如果count <= mid:
+    
+        # 那么
+        
         left = 1
         right = len(nums) - 1 # n + 1 - 1 = n
         
@@ -35,5 +45,6 @@ class Solution(object):
                 left = mid + 1 
             else:
                 right = mid
+        
         return left 
         
