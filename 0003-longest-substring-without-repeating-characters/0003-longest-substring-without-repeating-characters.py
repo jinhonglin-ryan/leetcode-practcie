f7bbfbab510d9eb1398d3e86a4ad1c5c5a0ff40e
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-25 18:21:52 -0400

    Time: 89 ms (25.32%), Space: 11.8 MB (92.66%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,25 +5,20 @@ class Solution(object):
         :rtype: int
         """
         
-        left = 0
+        left = 0 
         right = 0
-        window = {}
+        s_window = Counter(s)
+        curr_window = Counter()
         max_len = 0
         
-        while right < len(s):
-            if s[right] not in window:
-                window[s[right]] = 1
-            else: 
-                window[s[right]] += 1
         
-            while window[s[right]] > 1:
-                window[s[left]] -= 1
+        while right < len(s):
+            curr_window[s[right]] += 1
+            
+            while curr_window[s[right]] > 1:
+                curr_window[s[left]] -= 1
                 left += 1
                 
-            max_len = max(max_len, right - left + 1)   
+            max_len = max(max_len, right - left + 1)    
             right += 1
-        return max_len
-                
-                    
-                
-    
+        return max_len 
