83e73b846e709578f7b72c38da4b53f7bd174048
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-25 15:39:14 -0400

    Time: 51 ms (46.30%), Space: 19.7 MB (50.42%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -14,27 +14,39 @@ class Solution(object):
         :rtype: TreeNode
         """
         
-        if root is None or root == p or root == q:
-            return root
+#         # Method 1: Recursion
+#         if root is None or root == p or root == q:
+#             return root
     
-        # 递归在左子树搜索
-        left = self.lowestCommonAncestor(root.left, p, q) if root.left else None
-        # 递归在右子树搜索
-        right = self.lowestCommonAncestor(root.right, p, q) if root.right else None
+#         # 递归在左子树搜索p or q
+#         left = self.lowestCommonAncestor(root.left, p, q) if root.left else None
+#         # 递归在右子树搜索p or q
+#         right = self.lowestCommonAncestor(root.right, p, q) if root.right else None
 
-        # 如果在左右子树中都找到了目标节点，则当前root是LCA
-        if left and right:
-            return root
+#         # 如果在左右子树中都找到了目标节点，则当前root是LCA
+#         if left and right:
+#             return root
 
-        # 如果只在左子树找到，则返回左子树的结果
-        if left:
-            return left
+#         # 如果只在左子树找到，则返回左子树的结果
+#         if left:
+#             return left
 
-        # 如果只在右子树找到，则返回右子树的结果
-        if right:
-            return right
+#         # 如果只在右子树找到，则返回右子树的结果
+#         if right:
+#             return right
 
-        # 如果两边都没有，则返回None
-        return None
+#         # 如果两边都没有，则返回None
+#         return None
 
-
+        # Method 2: Iterative
+        lca = root
+        
+        while lca:
+            if lca.val > p.val and lca.val > q.val:
+                lca = lca.left
+            elif lca.val < p.val and lca.val < q.val:
+                lca = lca.right
+            else:
+                break
+        return lca
+        
