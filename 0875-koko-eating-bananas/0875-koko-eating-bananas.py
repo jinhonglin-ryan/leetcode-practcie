7176e010def1c8f8aadc14cd0f859eb309af2cd7
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-25 18:17:24 -0400

    Time: 241 ms (94.50%), Space: 12.9 MB (62.61%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,21 +1,39 @@
 class Solution(object):
     
-    def can_eat(self, piles, speed, hours):
+    def check_if_can_eat(self, piles, hour, speed):
+        # 检查是否可以在 mid(speed) 速度下在 h 小时内吃完所有香蕉：
         time = 0
+        # 计算在当前speed下可否在h小时内吃完
         for pile in piles:
-            time += (pile + speed - 1) // speed
-        return time <= hours
+            time += (pile + speed - 1) // speed 
+            
+        return time <= hour
+    
     
     def minEatingSpeed(self, piles, h):
-        left = 1
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+        
+        # left 和 right是eating speed 我们在这个范围内找最小的eating speed
+        # left是1是因为eating speed是integer，最小的eating speed就只能是1
+        # right是max(piles)是最大的那一堆里面的香蕉数量，因为我们如果eating speed 大于这个最大值，我们在一个小时吃完后也不会去吃别的，所以eating speed in that case is larger than necessary
+        left = 1 
         right = max(piles)
         
+        
+        # 在这个left - right speed range里面找最小值 
         while left < right:
+            # 计算中间速度, 检查是否可以在 mid 速度下在 h 小时内吃完所有香蕉：
             mid = left + (right - left) // 2
-            
-            if not self.can_eat(piles, mid, h):
+            if not self.check_if_can_eat(piles, h, mid):
+                # 如果can eat function 返回False，说明速度 mid 太慢，无法在 h 小时内吃完所有香蕉，需要增加速度。因此，将 left 更新为 mid + 1。
                 left = mid + 1
             else:
+                # 如果可以吃完，说明速度 mid 可以在 h 小时内吃完所有香蕉，甚至可能更慢的速度也可以。因此，将 right 更新为 mid, 保留当前的 mid 速度，并继续在更小的范围内搜索更低的可能的速度。
                 right = mid
-        
+                
+        # 当 left 等于 right 时，循环结束。此时，left 和 right 都指向了可以在 h 小时内吃完所有香蕉的最小速度。
         return left
