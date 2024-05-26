93f00839316fc7b1eb9ee11b756169d213cfd892
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-25 23:08:13 -0400

    Time: 15 ms (91.72%), Space: 12.4 MB (77.29%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -12,19 +12,21 @@ class Solution(object):
         """
         if root is None:
             return None
+        
+        # res 用于存储整个树的层序遍历结果
         res = []
         
+        # queue 用于按顺序存储待处理的树节点
         queue = [root]
         
         while queue:
+            # 用于存储当前层的所有节点的值
             level = []
-            
             n = len(queue)
-            
             for _ in range(n):
                 node = queue.pop(0)
                 level.append(node.val)
-                
+                # 保证了下一层的节点将在下一个大循环中按顺序处理
                 if node.left:
                     queue.append(node.left)
                     
