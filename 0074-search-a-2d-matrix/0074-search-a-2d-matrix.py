a0c13a4243d1e836162ff8d30bdf646fb1016a91
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-26 03:12:41 -0400

    Time: 28 ms (28.40%), Space: 12 MB (82.97%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -8,22 +8,31 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-        def binary_search(nums, target):
-            left = 0
-            right = len(nums) - 1
-
-            while left <= right: 
-                mid = (left + right) // 2
-
-                if nums[mid] == target:
-                    return True
-                elif nums[mid] > target: 
-                    right = mid - 1
-                else:
-                    left = mid + 1 
-            return False
-    
-        for row in matrix:
-            if binary_search(row, target):
-                return True
-        return False
+        left = 0
+        right = len(matrix) - 1 
+        while left <= right:
+            mid = left + (right - left) // 2
+            
+            if matrix[mid][0] <= target <= matrix[mid][-1]:
+                left_2 = 0
+                right_2 = len(matrix[mid]) - 1
+                
+                while left_2 <= right_2:
+                    mid_2 = left_2 + (right_2 - left_2) // 2
+                    if matrix[mid][mid_2] == target:
+                        return True
+                    elif matrix[mid][mid_2] < target:
+                        left_2 = mid_2 + 1
+                        continue
+                    else:
+                        right_2 = mid_2 - 1
+                        continue 
+                        
+                return False
+            elif matrix[mid][0] > target:
+                right = mid - 1
+                continue
+            else:
+                left = mid + 1
+                continue
+        return False 
