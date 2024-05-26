497013138e04e8352516529f84fe1a70a9bf57c7
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-26 02:34:27 -0400

    Time: 256 ms (5.67%), Space: 47.5 MB (57.60%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -5,20 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def __init__(self):
-        self.count = 0
-        
-    def helper(self, node, max_val):
+    def dfs(self, node, max_val):
         if node is None:
-            return
+            return 0
         
+        count = 0
         if node.val >= max_val:
-            self.count += 1
+            count += 1
             max_val = node.val
         
-        self.helper(node.left, max_val)
-        self.helper(node.right, max_val)
-        
+        count += self.dfs(node.left, max_val) + self.dfs(node.right, max_val)
+        return count
         
         
     def goodNodes(self, root):
@@ -26,11 +23,9 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        return self.dfs(root, root.val)
         
-        if root is None:
-            return 0
-        self.helper(root, root.val)
-        return self.count 
-        
-        
+    # dfs: 从根节点出发，遍历完所有的节点，能找到好节点的数量
+    # dfs(None, max_val) = 0
+    # dfs(u, max_val) = 本身是否是一个好节点 + dfs(u.left, max_val) + dfs(u.right, max_val)
         
