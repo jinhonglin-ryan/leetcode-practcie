8c5d219bf30c783b122e54784854e52e1afc462f
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-26 02:53:01 -0400

    Time: 16 ms (43.68%), Space: 11.7 MB (41.22%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -13,6 +13,18 @@ class Solution(object):
         :rtype: bool
         """
         
+        if p is None and q is None:
+            return True
+        
+        if not p or not q:
+            return False
+        
+        if p.val != q.val:
+            return False 
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        
         if not p and not q:
             return True
         if not p or not q:
@@ -20,6 +32,3 @@ class Solution(object):
         if p.val != q.val:
             return False
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        
-        
-        
