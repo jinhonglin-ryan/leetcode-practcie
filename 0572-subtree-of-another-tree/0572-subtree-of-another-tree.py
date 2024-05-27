30211c7bd4068ae1967266efa5e26c3caba8cc95
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-27 03:28:14 -0400

    Time: 113 ms (17.67%), Space: 12.8 MB (38.45%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -5,16 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    
+    # 两棵树相等的条件：
+    # 1. 根结点相等
+    # 2. 并且 左子树等于左子树
+    # 3. 并且 右子树等于右子树
     def isSameTree(self, p, q):
         if p is None and q is None:
             return True
         
-        if (p is None and q is not None) or (p is not None and q is None) or p.val != q.val: 
+        if (p is None and q is not None) or (p is not None and q is None):
             return False
         
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
     
-    
+    # subRoot是root子树的条件：
+    # 1. subRoot 和 root 两棵树完全一样
+    # 2. 或者 subRoot 和 root的左子树一样
+    # 3. 或者 subRoot 和 root的右子树一样 
     def isSubtree(self, root, subRoot):
         """
         :type root: TreeNode
