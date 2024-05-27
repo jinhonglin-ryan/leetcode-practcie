efa1f7a167e0064ff4048e3b3233511a187d9f1f
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-27 04:01:33 -0400

    Time: 388 ms (63.71%), Space: 11.6 MB (91.46%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -7,12 +7,20 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        
+        # 中心扩展法：这种方法对每一个可能的中心位置进行扩展，尝试找出最长的回文子串。
+        # 对于输入字符串中的每个字符，都尝试将其作为回文的中心，并向两边扩展。
+        # 分别检查以该字符为中心的奇数长度回文（如 "aba"）和偶数长度回文（如 "abba"）。
+        
         if not s:
             return ""
         
         max_len = 0
         longest_pal = s[0]  # 初始化为第一个字符，保证至少有一个字符返回
-
+        
+        # 使用两个指针 l（left）和 r（right），初始化为当前中心位置。
+        # 对于奇数长度，两个指针都从 i 开始；
+        # 对于偶数长度，l 从 i 开始，r 从 i + 1 开始。
         for i in range(len(s)):
             # Explore palindromes of odd length
             l, r = i, i
