9af792e04cedd3bd0875b647a9ed1fe87a6bd31e
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-28 02:55:41 -0400

    Time: 21 ms (11.15%), Space: 11.8 MB (28.36%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -14,20 +14,14 @@ class Solution(object):
         dummy.next = head
         prev = dummy
         
-        # 只有当这两个节点都存在时，我们才有一对需要交换的节点
-        # 每次循环处理一对节点，然后将 prev 指针移动到这对节点之后，即交换过的第二个节点的位置，为处理下一对节点做准备。
         while prev.next and prev.next.next:
             curr = prev.next
-            next_node = curr.next 
+            next_node = curr.next
             
-            # swap
             curr.next = next_node.next
-            next_node.next = curr 
-            # prev指的是当前交换对的前一个node，所以prev.next要指向交换完成后的那个node
+            next_node.next = curr
             prev.next = next_node
             
-            
-            # 在交换之后，原来的 curr 节点现在是这对节点中的第二个节点。因此，将 prev 更新为 curr，这样在下一次循环中，prev.next 将指向新的 curr，即下一对待交换节点的第一个节点。
             prev = curr
-            
+        
         return dummy.next 
