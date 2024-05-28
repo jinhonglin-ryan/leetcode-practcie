9254b60fc7afb9cdad9f628febc94aa9f2b7606c
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-28 04:49:55 -0400

    Time: 595 ms (40.67%), Space: 18 MB (78.00%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -5,8 +5,9 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
+        # 注意：在使用 heapq.heappush() 和 heapq.heappop() 的过程中，堆操作是基于元组中的第一个元素来进行的，这里第一个元素是距离的平方值，而不是索引。
         
-        # 创建一个最小堆，存储每个点及其距离的平方
+        # 创建一个最小堆，存储每组点到原点的距离的平方，和对应在原先数组中的index
         min_heap = []
         for index, (x, y) in enumerate(points):
             distance_squared = x**2 + y**2
