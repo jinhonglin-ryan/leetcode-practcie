9d51c074c45adc77d664b0818bd5ce49cff40300
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-05-28 23:40:19 -0400

    Time: 53 ms (83.32%), Space: 16.5 MB (89.10%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -5,22 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    # pl pr 定义了preorder里面的左右端点
-    # il ir 定义了inorder里面的左右端点
-    # 然后进行recursion
-    def build(self, preorder, inorder, pl, pr, il, ir):
+    
+    def build(self, preorder, inorder, pl, pr, il, ir): 
         if pl > pr or il > ir:
             return None
-        # 创建一个根节点，根节点永远是preorder的left端点
-        root = TreeNode(preorder[pl])
         
-        # k is the root index
-        k = inorder.index(root.val)
+        root_val = preorder[pl]
+        k = inorder.index(root_val)
+        root = ListNode(val=root_val)
         
         root.left = self.build(preorder, inorder, pl + 1, pl + 1 + k - 1 - il, il, k - 1)
-        root.right = self.build(preorder, inorder, pl + 1 + k - 1 - il + 1, pr,k + 1, ir)
+        root.right = self.build(preorder, inorder, pl + 1 + k - 1 - il + 1, pr, k + 1, ir)
         
         return root
+        
         
     def buildTree(self, preorder, inorder):
         """
@@ -28,8 +26,7 @@ class Solution(object):
         :type inorder: List[int]
         :rtype: TreeNode
         """
-        return self.build(preorder, inorder, 0, len(preorder) - 1, 0, len(inorder) - 1)
         
-        
+        return self.build(preorder,inorder, 0, len(preorder) - 1, 0, len(inorder) - 1)
         
     
