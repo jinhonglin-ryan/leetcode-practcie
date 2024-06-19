fac891f379662f05e4ebbec7023363bf15b89898
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-06-19 15:03:44 -0400

    Time: 87 ms (9.06%), Space: 11.6 MB (63.76%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -8,9 +8,10 @@ class Solution(object):
         
         ans = []
         path = []
-        used = [False for _ in candidates]
-        candidates = sorted(candidates)
+        used = [False for _ in candidates] # 为了进行去重 需要知道哪些数被用过
+        candidates = sorted(candidates) # 要进行去重的话，要在树层上去重，需要将candidates排序
         
+        # startIndex 用于保证每一个数只用一次
         def backtrack(candidates, target, path, startIndex):
             if sum(path) == target:
                 ans.append(path[:])
@@ -18,6 +19,8 @@ class Solution(object):
                 return
             
             for i in range(startIndex, len(candidates)):
+                # 树层上的去重处理 树枝上不去重
+                # 如果当前的number 跟前一个number是一样的，并且前一个number没被用，是树层上的重复，需要去重
                 if i > 0 and candidates[i - 1] == candidates[i] and used[i - 1] == False:
                     continue
                 path.append(candidates[i])
