4e810463861d6584c0bbb21b20ce2d69880b7699
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-06-19 15:03:31 -0400

    Time: 24 ms (22.80%), Space: 11.9 MB (16.63%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -7,8 +7,8 @@ class Solution(object):
         
         ans = []
         path = []
-        used = [False for _ in nums]
-        nums = sorted(nums)
+        used = [False for _ in nums] # 为了进行去重 需要知道哪些数被用过
+        nums = sorted(nums) # 要进行去重的话，要在树层上去重，需要将nums去重
         
         def backtrack(nums, path, used, startIndex):
             ans.append(path[:])
@@ -16,7 +16,9 @@ class Solution(object):
             if startIndex >= len(nums): 
                 return
             
+            # startIndex 用于保证每一个数只用一次
             for i in range(startIndex, len(nums)):
+                # 如果当前的number 跟前一个number是一样的，并且前一个number没被用，是树层上的重复，需要去重
                 if (i > 0) and (nums[i - 1] == nums[i]) and (used[i - 1] == False): # 对树枝不去重，对树层上的去重
                     continue
                     
