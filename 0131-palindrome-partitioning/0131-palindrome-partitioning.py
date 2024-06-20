0cb656b5c8eaf99a3b9f3bd7b009a86573a7f6f5
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-06-20 03:28:10 -0400

    Time: 475 ms (60.47%), Space: 30.9 MB (38.02%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -8,15 +8,15 @@ class Solution(object):
         ans = []
         path = []
         def backtrack(startIndex, path):
-            if startIndex == len(s):
+            if startIndex == len(s): # 如果startIndex走到末尾了，将path加入ans
                 ans.append(path[:])
                 return
             
             for i in range(startIndex, len(s)):
                 if s[startIndex:i+1] == s[startIndex:i+1][::-1]: # 如果是palindrome
-                    path.append(s[startIndex:i+1])
-                    backtrack(i+1, path)
-                    path.pop()
+                    path.append(s[startIndex:i+1]) # 则将这一段内容加入path
+                    backtrack(i+1, path) # 然后在i+1个位置开始往后面找palindrome
+                    path.pop() # 恢复path 
                     
         backtrack(0, path)
         return ans 
