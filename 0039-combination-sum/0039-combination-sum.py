edc47526e0bd3dfcaea749d2dce1cf5b4134cbe1
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-06-20 02:40:27 -0400

    Time: 50 ms (22.13%), Space: 11.6 MB (69.74%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -13,9 +13,9 @@ class Solution(object):
             if sum(path) > target:
                 return
             
-            for i in range(startIndex, len(nums)):
+            for i in range(startIndex, len(nums)): # 我们要避免1，2 和 2，1的重复，所以要从startIndex之后去找
                 path.append(nums[i])
-                backtrack(nums, path, target, i)
+                backtrack(nums, path, target, i) # 因为这里说一个数字可以用无数次，所以下一步还是可以从这个数字里找
                 path.pop()
         
         backtrack(candidates, path, target, 0)
