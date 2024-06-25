f6b50551fc2afc398ceab54dd3cb496730717035
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-06-25 14:58:36 +0800

    Time: 34 ms (37.65%), Space: 11.8 MB (92.46%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -14,25 +14,22 @@ class Solution(object):
         :type node: Node
         :rtype: Node
         """
+        
         if node is None:
             return None
         
-        # 深度优先搜索复制所有节点
         self.dfs(node)
         
-        # 将旧节点的邻居复制到新节点
-        for old_node, new_node in self.old_to_new.items():
-            for neighbor in old_node.neighbors:
-                new_node.neighbors.append(self.old_to_new[neighbor])
-        
-        # 返回新图的起始节点
+        for old, new in self.old_to_new.items():
+            for neighbor in old.neighbors:
+                new.neighbors.append(self.old_to_new[neighbor])
+                
         return self.old_to_new[node]
     
     def dfs(self, node):
-        # 创建新节点并记录在字典中
         self.old_to_new[node] = Node(node.val)
         
-        # 递归地复制邻居节点
         for neighbor in node.neighbors:
             if neighbor not in self.old_to_new:
                 self.dfs(neighbor)
+       
