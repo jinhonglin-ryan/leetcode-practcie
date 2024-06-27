f2db2e4ca50bd196a3c80adea507efe52bc5127b
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-06-27 14:32:18 +0800

    Time: 62 ms (88.71%), Space: 12.9 MB (88.28%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,47 @@
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        
+        edges = prerequisites
+        n = numCourses
+        
+        g = defaultdict(list)
+        inDegrees = [0 for _ in range(n)]
+        
+        # 或者可以写 g = dict()
+        # for i in range(n):
+        #     g[i] = []
+        
+        for e in edges:
+            a = e[0]
+            b = e[1]
+            g[b].append(a)
+            inDegrees[a] += 1
+        
+        queue = deque()
+        
+        for i in range(n):
+            if inDegrees[i] == 0:
+                queue.append(i)
+        
+        count = 0
+        ans = []
+        while queue:
+            t = queue.popleft()
+            count += 1
+            ans.append(t)
+            for v in g[t]:
+                inDegrees[v] -= 1
+                if inDegrees[v] == 0:
+                    queue.append(v)
+        
+        if count != numCourses:
+            return []
+        return ans 
+        
+        
+            
