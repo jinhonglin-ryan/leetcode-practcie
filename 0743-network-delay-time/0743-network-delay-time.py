13f35ace20ad83239a3d89a1e1405fa631680a6f
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-02 03:30:26 +0800

    Time: 327 ms (93.68%), Space: 13.7 MB (85.38%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -3,8 +3,6 @@ class Solution(object):
     # 求单源最短路径，即从一个指定起点（源点）到图中其他所有顶点的最短路径。
     # 适用于加权图，但要求图中的边权重为非负值。
     # 通过贪心策略，不断选择当前最短路径的顶点，并更新与之相邻的顶点的最短路径。
-    
-    
     def networkDelayTime(self, times, n, k):
         """
         :type times: List[List[int]]
@@ -15,49 +13,97 @@ class Solution(object):
         
         graph = defaultdict(list)
         
-        for e in times:
-            u = e[0]
-            v = e[1]
-            t = e[2]
-            
-            graph[u].append([v, t])
-            
-        dist = self.dijkstra(graph, k, n)
-        max_dist = 0
+        for u, v, t in times:
+            graph[u].append((v, t))
         
-        for i in range(1, n + 1):
-            if dist[i] == float('inf'):
-                return -1
-            max_dist = max(max_dist, dist[i])
-        return max_dist
+        distance = self.dijkstra(graph, n, k)
+        max_dist = max(distance.values())
+        return max_dist if max_dist < float('inf') else -1
     
     
-    def dijkstra(self, graph, start, n):
-        
-        dist = [float('inf') for _ in range(n + 1)]
-        
+    def dijkstra(self, graph, n, start):
+        # Priority queue: (distance, node)
+        pq = [(0, start)]
+        dist = {i: float('inf') for i in range(1, n + 1)} # 记录从起点出发到每个点到最小距离
         dist[start] = 0
         
-        queue = deque()
-        queue.append([start, 0])
-        
-        while queue:
-            v, time = queue.popleft()
-            if time > dist[v]:
+        while pq:
+            current_dist, u = heapq.heappop(pq)
+            
+            if current_dist > dist[u]:
                 continue
             
-            dist[v] = time
-            
-            if graph[v] == []:
-                continue
-            
-            for e in graph[v]:
-                q = e[0]
-                t = e[1]
-                
-                if dist[q] > t + time:
-                    dist[q] = t + time
-                    queue.append([q, dist[q]])
+            for v, time in graph[u]:
+                new_dist = current_dist + time
+                if new_dist < dist[v]:
+                    dist[v] = new_dist
+                    heapq.heappush(pq, (new_dist, v))
         return dist
+        
+        
+        
+    
+#     def networkDelayTime(self, times, n, k):
+#         """
+#         :type times: List[List[int]]
+#         :type n: int
+#         :type k: int
+#         :rtype: int
+#         """
+#         # 初始化图
+#         # graph这个dictionary 存的是从key出发可以到哪些点，花费时间是多少
+#         # 到哪些点是value[0]，到该点花费时间是value[1]
+#         # graph[u] 存储从节点 u 出发可以到达的节点 v 及其对应的权重 t。
+#         graph = defaultdict(list)
+#         for e in times:
+#             u = e[0]
+#             v = e[1]
+#             t = e[2]
             
+#             graph[u].append([v, t])
+        
+#         # 计算从起点 k 到所有节点的最短路径，返回的 dist 数组表示从起点到每个节点的最短路径长度。
+#         dist = self.dijkstra(graph, k, n)
+        
+#         max_dist = 0
+#         for i in range(1, n + 1):
+#             if dist[i] == float('inf'): # 如果某个节点的最短路径是无穷大，说明我们没有更新到这个节点，仍然是初始的无穷大值，说明该节点无法到达，返回 -1。
+#                 return -1
+#             max_dist = max(dist[i], max_dist)
+#         return max_dist
+    
+    
+#     def dijkstra(self, graph, start, n):
+#         dist = [float('inf') for _ in range(n + 1)] # 先初始化dis，存放从起点到该点到最短距离 
+#         # queue表示对到某一个点的距离进行修改后，需要把这个点取出，从当前节点出发，尝试更新到其他点的最短距离
+#         queue = deque()
+        
+#         # 从起点到起点到最短距离是0
+#         dist[start] = 0
+        
+#         queue.append([start, 0])
+        
+#         while queue:
+#             target, time = queue.popleft()
+#             # 如果当前到target所需要的时间大于已知的到target所需要的最短时间，跳过
+#             if time > dist[target]:
+#                 continue 
             
+#             # 否则 更新到target的最短时间为当前时间
+#             dist[target] = time
+            
+#             # 如果当前target没有指向的节点，跳过
+#             if graph[target] == []:
+#                 continue
+            
+#             for e in graph[target]: # 遍历当前节点的所有邻接点
+#                 v = e[0]
+#                 t = e[1]
+#                 if (dist[v] > t + time): # 如果从当前节点到邻接点的路径长度小于已知的最短路径长度，更新 dist[v]。
+#                     dist[v] = t + time 
+#                     # 并且将这个节点加入队列，晚点需要pop出来来更新从这个节点出发到别的节点的最短路径
+#                     queue.append([v, dist[v]])
+                
+#         return dist
+        
+        
