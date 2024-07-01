5466c0fe743f2472a25b96031e1ce33f3b4a052d
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-01 18:38:54 +0800

    Time: 29 ms (89.83%), Space: 11.4 MB (99.64%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -6,19 +6,22 @@ class Solution(object):
         """
         
         # dp 问题
-        # f[i] 表示跳到i的最小cost
-        # 这可被分成两个子问题：到第 i - 1 个台阶所花费的最小cost 与 到第 i - 2 个台阶所花费的最小cost中的 最小值 + 到达第 i 个台阶所需要的cost。
-        # f[i] = min(f[i - 1], f[i - 2]) + cost[i]
+        # 跳到 下标 0 或者 下标 1 是不花费体力的， 从 下标 0 下标1 开始跳就要花费体力了，分别花费为cost[0] 和 cost[1]
+        # dp[i] 表示跳到第i台阶所花费的最少体力
+        # 这可被分成两个子问题：从i-1和i-2的位置跳
+        # dp[i - 1] 跳到 dp[i] 需要花费 dp[i - 1] + cost[i - 1]。dp[i - 2] 跳到 dp[i] 需要花费 dp[i - 2] + cost[i - 2]。
+        # dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
         
         n = len(cost)
         
-        f = [0 for _ in range(n)]
+        f = [0 for _ in range(n + 1)]
+
+        f[0] = 0
+        f[1] = 0
+    
+        for i in range(2, n + 1):
+            f[i] = min(f[i - 1] + cost[i - 1], f[i - 2] + cost[i - 2])
         
-        f[0] = cost[0]
-        f[1] = cost[1]
-        
-        for i in range(2, n):
-            f[i] = min(f[i - 1], f[i - 2]) + cost[i]
-            
-        return min(f[n - 2], f[n - 1])
+        # 
+        return f[n]
         
