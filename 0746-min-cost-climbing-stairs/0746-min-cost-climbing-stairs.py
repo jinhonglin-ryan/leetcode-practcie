4464b660ea2655c992930eb25c87cf3703a50b85
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-01 18:39:03 +0800

    Time: 34 ms (63.55%), Space: 11.7 MB (76.51%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -21,7 +21,6 @@ class Solution(object):
     
         for i in range(2, n + 1):
             f[i] = min(f[i - 1] + cost[i - 1], f[i - 2] + cost[i - 2])
-        
-        # 
+
         return f[n]
         
