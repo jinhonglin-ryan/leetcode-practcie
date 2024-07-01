bb621896c32aa46b56034236694fbd78aae20524
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-02 04:03:47 +0800

    Time: 2655 ms (68.48%), Space: 118.9 MB (53.52%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -8,30 +8,56 @@ class Solution(object):
         :rtype: bool
         """
         
+#         # DFS
+#         graph = defaultdict(list)
+#         visited = set()
+#         for a, b in edges:
+#             graph[a].append(b)
+#             graph[b].append(a)
+            
+#         def dfs(node):
+#             if node == destination:
+#                 return True
+            
+#             if node in visited:
+#                 return False
+            
+#             visited.add(node)
+            
+#             for neighbor in graph[node]:
+#                 if dfs(neighbor):
+#                     return True
+#             return False
+        
+#         return dfs(source)
+    
+        # BFS
         graph = defaultdict(list)
-        visited = set()
         for a, b in edges:
             graph[a].append(b)
             graph[b].append(a)
             
-        def dfs(node):
-            if node == destination:
-                return True
+        
+        queue = deque()
+        queue.append(source)
+        visited = set()
+        
+        while queue:
+            node = queue.popleft()
             
             if node in visited:
-                return False
+                continue 
+            
+            if node == destination:
+                return True
             
             visited.add(node)
             
             for neighbor in graph[node]:
-                if dfs(neighbor):
-                    return True
-            return False
+                if neighbor not in visited:
+                    queue.append(neighbor)
         
-        return dfs(source)
-    
-
-
+        return False 
             
         
         
