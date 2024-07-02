777272c1c4465dcb690a7a80975fff5b8ce6b7bb
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-03 02:30:02 +0800

    Time: 4462 ms (18.83%), Space: 20 MB (6.55%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -5,6 +5,9 @@ class Solution(object):
         :rtype: str
         """
         
+        # Method 1: DP
+        # dp[i][j] 表示s在[i:j+1]中是不是一个palindrome
+        
         n = len(s)
         
         if n == 0:
@@ -15,28 +18,36 @@ class Solution(object):
         
         dp = [[False for _ in range(n)] for _ in range(n)]
         
+        # 对于单个字符而言，其肯定是回文串，因此初始化dp为如下
         for i in range(n):
             dp[i][i] = True
             
         max_len = 1
         max_len_start = 0
         
-        for length in range(1, n + 1):
-            for i in range(n - length + 1):
+        # 区间DP的写法：外层枚举长度，内层枚举左端点
+        for length in range(1, n + 1): # 子串长度从 1 到 n
+            for i in range(n - length + 1): # 枚举左端点位置
                 # j - i + 1 = length
-                j = length + i - 1
+                j = length + i - 1 # 子串结束位置
                 
+                # 如果当前的两个端点相同
                 if s[i] == s[j]:
+                    # 如果字符串的大小（包含i和j的两个端点）为1，2，或者3
+                    # 那么该字符串肯定是回文串
+                    # 比如a, aa, aba, 分别对应i和j指向同一个字符，i和j中间没有隔任何字符，i和j中间只隔了一个字符，这都是回文串
                     if length <= 3:
                         dp[i][j] = True
-                    else:
-                        dp[i][j] = dp[i + 1][j - 1]
-                
+                    else: # 处理更长的字符串的情况
+                        dp[i][j] = dp[i + 1][j - 1] # 我们外层循环按照长度枚举可以确保这里的依赖关系是成立的 dp[i][j] = dp[i + 1][j - 1] 即 dp[i + 1][j - 1]是在前面外层循环计算过的（即更短的长度的时候的循环）
+                        
+                # 如果当前是回文串，且长度大于最大的，更新
                 if dp[i][j] and length > max_len:
                     max_len = length
                     max_len_start = i
         return s[max_len_start: max_len_start + max_len]
-                    
+    
+                        
         
 #         # Method 2: 
 #         # 中心扩展法：这种方法对每一个可能的中心位置进行扩展，尝试找出最长的回文子串。
