444e9065e0bf40f83c59cdbc11535fb98371c32a
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-02 12:43:53 +0800

    Time: 4 ms (98.58%), Space: 11.6 MB (64.19%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -5,34 +5,70 @@ class Solution(object):
         :rtype: int
         """
         
-        # 与198类似，只不过多个分类讨论，现在是一个环形
-        # 198说的是在[0, n-1]的最大值
-        # 1. 第一个位置必选：则最后一个位置必不能选：返回的是g1[n - 1]
-        # 2. 第一个位置不选：则最后一个位置可选可不选：返回的是 max(f2[n - 1], g2[n -1])
-        # 则这道题是要考虑在[0, n-2]的最大值（选第一个的情况）
-        # 和[1, n-1]的最大值（不选第一个情况）
-        
         n = len(nums)
-        if n == 1:
-            return nums[0]
         if n == 0:
             return 0
+        if n == 1:
+            return nums[0]
         
-        def rob_helper(start, end):
-            f = [0 for _ in range(n)]
-            g = [0 for _ in range(n)]
+        f1 = [0 for _ in range(n)]
+        g1 = [0 for _ in range(n)]
+        
+        f1[1] = nums[1]
+        g1[1] = 0
+        
+        # 不选第一个
+        for i in range(2, n):
+            f1[i] = g1[i - 1] + nums[i]
+            g1[i] = max(f1[i-1], g1[i-1])
             
-            f[start] = nums[start]
-            g[start] = 0 
+        rob_first = max(f1[n - 1], g1[n - 1])
+        
+        # 选第一个
+        
+        f0 = [0 for _ in range(n)]
+        g0 = [0 for _ in range(n)] 
+        f0[0] = nums[0]
+        g0[0] = 0
+        
+        for i in range(1, n - 1):
+            f0[i] = g0[i - 1] + nums[i]
+            g0[i] = max(f0[i-1], g0[i-1])
             
-            for i in range(start + 1, end + 1):
-                f[i] = g[i - 1] + nums[i]
-                g[i] = max(f[i-1], g[i-1])
+        not_rob_first = max(f0[n-2], g0[n-2])
+        
+        return max(rob_first, not_rob_first) 
+        
+        
+        # 与198类似，只不过多个分类讨论，现在是一个环形
+        # 198说的是在[0, n-1]的最大值，这道题的分类讨论如下
+        # 1. 选第一个：考虑在[0, n-2]的最大值
+        # 2. 不选第一个：考虑在[1, n-1]的最大值
+        
+#         n = len(nums)
+#         if n == 1:
+#             return nums[0]
+#         if n == 0:
+#             return 0
+        
+#         def rob_helper(start, end):
+#             f = [0 for _ in range(n)]
+#             g = [0 for _ in range(n)]
+            
+#             f[start] = nums[start]
+#             g[start] = 0 
+            
+#             for i in range(start + 1, end + 1):
+#                 f[i] = g[i - 1] + nums[i]
+#                 g[i] = max(f[i-1], g[i-1])
                 
-            return max(f[end], g[end])
+#             return max(f[end], g[end])
         
-        rob_first = rob_helper(0, n - 2)
-        not_rob_first = rob_helper(1, n - 1)
+#         rob_first = rob_helper(0, n - 2)
+#         not_rob_first = rob_helper(1, n - 1)
         
-        return max(rob_first, not_rob_first)
+#         return max(rob_first, not_rob_first)
+
+        
+        
             
