5fa903b1d14e46f6c526a02dcd08a708aebc8415
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-02 12:46:05 +0800

    Time: 21 ms (15.97%), Space: 11.5 MB (64.19%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -4,6 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # 与198类似，只不过多个分类讨论，现在是一个环形
+        # 198说的是在[0, n-1]的最大值，这道题的分类讨论如下
+        # 1. 选第一个：考虑在[0, n-2]的最大值
+        # 2. 不选第一个：考虑在[1, n-1]的最大值
         
         n = len(nums)
         if n == 0:
@@ -11,23 +15,25 @@ class Solution(object):
         if n == 1:
             return nums[0]
         
+        
+        # 分类讨论1：不选第一个，则在[1, n -1] 找最大值（最后一位可选可不选）
         f1 = [0 for _ in range(n)]
         g1 = [0 for _ in range(n)]
         
         f1[1] = nums[1]
         g1[1] = 0
         
-        # 不选第一个
         for i in range(2, n):
             f1[i] = g1[i - 1] + nums[i]
             g1[i] = max(f1[i-1], g1[i-1])
             
         rob_first = max(f1[n - 1], g1[n - 1])
         
-        # 选第一个
         
+        # 分类讨论2：选第一个，则在[0, n - 2]找最大值 （最后一位必不选）
         f0 = [0 for _ in range(n)]
         g0 = [0 for _ in range(n)] 
+        
         f0[0] = nums[0]
         g0[0] = 0
         
