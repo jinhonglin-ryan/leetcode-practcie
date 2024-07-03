dbf2be1337ccb94c15ce952b2097a38ec5918269
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-04 01:21:17 +0800

    Time: 31 ms (10.65%), Space: 11.9 MB (40.70%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -6,23 +6,43 @@ class Solution(object):
         :rtype: bool
         """
         
-        # DFS解法
-        wordSet = set(wordDict)
-        memo = {}        
+#         # Method 1. DFS解法
+#         # 深度优先搜索（DFS）和记忆化（memoization）
+#         wordSet = set(wordDict)
+#         memo = {}        
         
-        def dfs(start):
-            if start == len(s):
-                return True
+#         def dfs(start):
+#             if start == len(s):
+#                 return True
             
-            if start in memo:
-                return memo[start]
+#             if start in memo:
+#                 return memo[start]
             
-            for end in range(start + 1, len(s) + 1):
-                if s[start: end] in wordSet and dfs(end):
-                    memo[start] = True
-                    return True
+#             for end in range(start + 1, len(s) + 1):
+#                 if s[start: end] in wordSet and dfs(end):
+#                     memo[start] = True
+#                     return True
                 
-            memo[start] = False
-            return False
+#             memo[start] = False
+#             return False
         
-        return dfs(0)
+#         return dfs(0)
+    
+    
+        # Method 2. 常规DP解法
+        # dp[i] 表示前i个字符可不可以被拆分成dict里的
+        # 返回dp[n]
+        
+        n = len(s)
+        
+        dp = [False for _ in range(n + 1)]
+        
+        dp[0] = True
+        
+        # 外层循环表示字串的结束位置
+        for i in range(1, n + 1):
+            for j in range(i): # 内层循环表示字串的起始位置
+                if s[j: i] in wordDict and dp[j]:
+                    dp[i] = True
+                    
+        return dp[n]
