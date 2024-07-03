5ea0cc6e1693f59c83c642e6e720b3dc4f47cfa3
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-04 01:04:09 +0800

    Time: 53 ms (42.20%), Space: 13.4 MB (5.11%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        # DP 问题 
+        # f[i] 表示的是以 nums[i] 结尾的所有子数组中，乘积最大的那个子数组的乘积。
+        # g[i] 表示的是以 nums[i] 结尾的所有子数组中，乘积最小的那个子数组的乘积。
+        
+        # 在第 i 步时，dp_max[i] 的值可以由以下三种情况决定：
+
+        # 1. 当前元素 nums[i] 本身。这种情况对应于从当前元素重新开始计算子数组。
+        # 2. f[i - 1] * nums[i]，即前一个位置的最大乘积子数组乘以当前元素。因为当前元素是正数，这种情况会让乘积变大。
+        # 3. g[i - 1] * nums[i]，即前一个位置的最小乘积子数组乘以当前元素。因为当前元素是负数，这种情况会让乘积变大。
+        
+        
+        n = len(nums)
+        
+        f = [0 for _ in range(n)]
+        g = [0 for _ in range(n)]
+        
+        f[0] = nums[0]
+        g[0] = nums[0]
+        res = nums[0]
+        
+        for i in range(1, n):
+            a = nums[i]
+            f[i] = max(f[i - 1] * a, a, g[i - 1] * a)
+            g[i] = min(f[i - 1] * a, a, g[i - 1] * a)
+            res = max(res, f[i])
+            
+        return res 
