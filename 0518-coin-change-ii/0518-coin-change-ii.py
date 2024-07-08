fddbd8875873a5095be500073aaaa16d8250ece7
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-08 17:11:12 +0800

    Time: 83 ms (70.15%), Space: 11.8 MB (81.82%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        
+        # DP 完全背包
+        # 外层遍历物品，内层遍历背包容量，从小到大
+        # dp[j]: 装满容量为j的背包，有dp[j]种方法
+        # 返回dp[amount]
+        
+        # 递推公式：dp[j] += dp[j - nums[i]]   (求装满背包有多少种方法 问题的公式)
+        
+        dp = [0 for _ in range(amount + 1)]
+        
+        dp[0] = 1
+        
+        for i in range(len(coins)):
+            for j in range(coins[i], amount + 1):
+                dp[j] += dp[j - coins[i]] 
+                
+                
+        return dp[amount]
+        
