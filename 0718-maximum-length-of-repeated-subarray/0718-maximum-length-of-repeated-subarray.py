f8f2f9828c4d38398b0bee824aa7816ea9573774
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-11 03:32:54 +0800

    Time: 1893 ms (26.67%), Space: 33.8 MB (15.00%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -6,8 +6,13 @@ class Solution(object):
         :rtype: int
         """
         
-        # dp[i][j] 为 nums1的前i个数 和 nums2的前j个数 重复子序列的最长长度
+        # dp[i][j] 为 以 nums1[i-1] 和 nums2[j-1] 结尾的最长公共子数组的长度。 
         # 返回dp数组中最大的值
+        # 而不是返回dp[n][m]: 直接返回 dp[n][m] 的假设是，最长的公共子数组会出现在 nums1 和 nums2 的结尾。然而，最长公共子数组可能出现在这两个数组的中间部分，而不是在结尾。
+        # 比如例子 nums1 = [12321] nums2 = [32147]
+        # dp[5][3] 就是考虑 12321 和 321 的重复子数组最长长度，就是3
+        # 而不是return dp[5][5], 因为公共子数组并不是出现在nums2的结尾
+        
         
         n = len(nums1)
         m = len(nums2)
@@ -18,7 +23,7 @@ class Solution(object):
         ans = 0 
         for i in range(1, n + 1):
             for j in range(1, m + 1):
-                # 当前元素可以构成公共子数组
+                # 表示当前元素可以延续之前的公共子数组。
                 if nums1[i - 1] == nums2[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1] + 1
                 else: # 当前元素不能构成公共子数组
@@ -27,5 +32,4 @@ class Solution(object):
                 # 找出最大值   
                 if dp[i][j] > ans:
                     ans = dp[i][j]
-                    
         return ans
