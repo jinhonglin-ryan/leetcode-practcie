d840ad02e577936ffea3cec72a44d45a77af8c27
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-10 16:01:26 +0800

    Time: 1905 ms (26.67%), Space: 33.9 MB (15.00%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         
         # dp[i][j] 为 nums1的前i个数 和 nums2的前j个数 重复子序列的最长长度
-        
+        # 返回dp数组中最大的值
         
         n = len(nums1)
         m = len(nums2)
@@ -18,9 +18,13 @@ class Solution(object):
         ans = 0 
         for i in range(1, n + 1):
             for j in range(1, m + 1):
+                # 当前元素可以构成公共子数组
                 if nums1[i - 1] == nums2[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1] + 1
+                else: # 当前元素不能构成公共子数组
+                    dp[i][j] = 0
                     
+                # 找出最大值   
                 if dp[i][j] > ans:
                     ans = dp[i][j]
                     
