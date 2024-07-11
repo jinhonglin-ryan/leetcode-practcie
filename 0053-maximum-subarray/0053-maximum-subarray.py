ad858963638da7ea9d5f216290280bdebe070c25
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-11 17:50:44 +0800

    Time: 540 ms (43.14%), Space: 24.6 MB (34.98%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -8,17 +8,16 @@ class Solution(object):
         # dp[i] 为以第 i 个元素结尾的子数组的最大和
         # 返回 max(dp)
         
+        # 对于每个元素 nums[i]，我们有两个选择：
+        # 1. 只包含 nums[i] 这个元素，作为一个新的子数组的开始。
+        # 2. 将 nums[i] 加入到前面计算出的最大子数组和中。
+        
         n = len(nums)
         dp = [0 for _ in range(n)]
         
         dp[0] = nums[0]
         
-        max_sum = nums[0]  # 初始化 max_sum 为 nums[0]
-
         for i in range(1, n):
-            # dp[i] 的值取决于 nums[i] 和 dp[i-1] + nums[i] 的较大值
-            dp[i] = max(nums[i], dp[i-1] + nums[i])
-            # 更新 max_sum
-            max_sum = max(max_sum, dp[i])
-
-        return max_sum
+            dp[i] = max(nums[i], dp[i - 1] + nums[i])
+            
+        return max(dp)
