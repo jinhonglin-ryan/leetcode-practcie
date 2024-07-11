1af8afa3d4a29a963ba4d607e2312bbb48d9af71
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-11 18:07:46 +0800

    Time: 50 ms (5.09%), Space: 12.8 MB (11.21%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -21,12 +21,14 @@ class Solution(object):
             
         for i in range(1, n + 1):
             for j in range(1, m + 1):
-                if s[i-1] == t[j-1]:
+                if s[i-1] == t[j-1]: # 如果 s 的第 i 个字符和 t 的第 j 个字符匹配。
+                    # 如果 s 的前 i-1 个字符是 t 的前 j-1 个字符的子序列，那么 s 的前 i 个字符就是 t 的前 j 个字符的子序列。
                     dp[i][j] = dp[i-1][j-1]
-                else:
-                    dp[i][j] = dp[i][j-1]
+                else: #  如果 s 的第 i 个字符和 t 的第 j 个字符不匹配。
+                    dp[i][j] = dp[i][j-1] # 我们不能将 t 的第 j 个字符包括在子序列中，那么我们需要看 t 的前 j-1 个字符是否可以包含 s 的前 i 个字符。如果 s 的前 i 个字符是 t 的前 j-1 个字符的子序列，那么 s 的前 i 个字符也会是 t 的前 j 个字符的子序列。继承下来，直到遍历到下一次 i
         
         return dp[n][m]
+        
         
         # # Method 2. 双指针
         # i, j = 0, 0
