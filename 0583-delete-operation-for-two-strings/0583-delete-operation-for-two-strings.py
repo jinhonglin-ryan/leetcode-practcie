16127b0a8cdc184651ff85f4b675cb9db5c19d71
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-12 16:27:13 +0800

    Time: 223 ms (24.63%), Space: 15.3 MB (21.64%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -37,5 +37,5 @@ class Solution(object):
                     dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 2)
                     
         return dp[n][m]
-            
-            
+    
+     
