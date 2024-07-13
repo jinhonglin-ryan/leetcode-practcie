98ca8b1d72c3c947e2e7348228412daecf5c1697
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-13 21:45:35 +0800

    Time: 85 ms (70.12%), Space: 12.7 MB (40.22%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -7,7 +7,7 @@ class Solution(object):
         # 贪心
         # 题目设定一定能跳到最远位置
         # 在覆盖范围内找能跳到的最远index
-        # 
+        # 同时记录下当前覆盖范围内，所能走到的最远距离，作为下一次的覆盖范围，count ++
         
         n = len(nums)
         
@@ -15,15 +15,18 @@ class Solution(object):
             return 0 
         
         curr_end = 0 # 记录下当前点的覆盖的最远距离
-        max_end = 0 # 记录下遍历到的最远距离 
+        next_end = 0 # 记录下遍历到的最远距离 
         count = 0 # 记录下用到的最小步骤
         
         for i in range(len(nums) - 1):
-            max_end = max(max_end, i + nums[i])
+            next_end = max(next_end, i + nums[i])
             
             if i == curr_end:
                 count += 1 
-                curr_end = max_end
+                curr_end = next_end
+                
+                if curr_end == len(nums) - 1:
+                    return count 
             
         return count 
         
