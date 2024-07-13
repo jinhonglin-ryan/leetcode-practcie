1b492b67018fdc34915edb126c5d4ad4c64aa41a
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-13 21:48:42 +0800

    Time: 83 ms (80.16%), Space: 12.7 MB (40.22%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         # 贪心
         # 题目设定一定能跳到最远位置
-        # 在覆盖范围内找能跳到的最远index
+        # 在覆盖范围内找能跳到的最远index（就是每次在「可跳范围」内选择可以使下一次跳的更远的位置。这样才能获得最少跳跃次数。）
         # 同时记录下当前覆盖范围内，所能走到的最远距离，作为下一次的覆盖范围，count ++
         
         n = len(nums)
@@ -18,12 +18,13 @@ class Solution(object):
         next_end = 0 # 记录下遍历到的最远距离 
         count = 0 # 记录下用到的最小步骤
         
-        for i in range(len(nums)):
+        # 不需要遍历到最后一个数，
+        for i in range(len(nums) - 1):
             next_end = max(next_end, i + nums[i])
             
-            if i == curr_end:
-                count += 1 
-                curr_end = next_end
+            if i == curr_end: # 到达当前跳跃范围的结束位置
+                count += 1 # 更新步数
+                curr_end = next_end # 更新下一次跳跃能到的最远位置
                 
                 # 早停
                 if curr_end == len(nums) - 1:
