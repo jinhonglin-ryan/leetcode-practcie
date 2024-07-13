c5731582102de61a5502d4b03f6feeb04a5f0a67
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-13 17:07:36 +0800

    Time: 372 ms (25.57%), Space: 12.8 MB (37.67%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -5,13 +5,17 @@ class Solution(object):
         :rtype: bool
         """
         
-        # 遍历nums数组，记录能跳到的最远位置
+        # 遍历nums数组，记录能跳到的最远下标
         max_reach = 0
+        
         for i in range(len(nums)):
+            # i是会到len(nums) - 1 这个最远下标的
             if max_reach < i :
                 return False
             
             max_reach = max(max_reach, i + nums[i])
+            
+            # 早停
             if max_reach == len(nums) - 1:
                 return True
             
