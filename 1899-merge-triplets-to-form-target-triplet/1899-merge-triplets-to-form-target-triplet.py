fad8e311bd23e1fd4dcbf7c70e3966bd2bc1a612
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-15 03:34:11 +0800

    Time: 1533 ms (80.67%), Space: 59.4 MB (16.81%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -11,9 +11,13 @@ class Solution(object):
                 return True 
         
         x, y, z = target
+        # 题目说了 triplets中都是>=1的正整数
         newa, newb, newc = 0, 0, 0
         
+        # max操作无顺序要求
         for a, b, c in triplets:
+            # 如果当前triplet内都是小于target的值，可以做max的操作
+            # 否则max会超过target，得不到结果
             if a <= x and b <= y and c <= z:
                 newa = max(a, newa)
                 newb = max(b, newb)
