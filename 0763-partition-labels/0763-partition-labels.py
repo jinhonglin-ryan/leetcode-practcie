98493683652376a2f1ad354e1a4c40102cb95f60
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-15 03:45:11 +0800

    Time: 19 ms (68.02%), Space: 11.6 MB (80.41%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -9,6 +9,7 @@ class Solution(object):
         num_last_index = {}
         
         for i in range(len(s)):
+            # 一直覆盖，获得最远下标
             num_last_index[s[i]] = i
         
         
@@ -17,8 +18,10 @@ class Solution(object):
         res = []
         
         for i in range(len(s)):
+            # 遍历s中的每个char，记录下当前区间的结束位置
             end = max(end, num_last_index[s[i]])
             
+            # 如果走到结束位置了，就记录下当前区间的长度，并且下一个区间的起点从 i + 1开始
             if i == end:
                 res.append(end - start + 1)
                 start = i + 1
