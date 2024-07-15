bc8d49f4d93abfb64c06f321ea261bb5f64c5c6e
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-16 04:04:41 +0800

    Time: 1064 ms (71.40%), Space: 58.2 MB (10.51%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -4,20 +4,37 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: int
         """
-        
-        intervals.sort(key=lambda x: (x[0], x[1]))
-        
-        # 第一个边界自动会进入else语句，count+=1变成0，开始计数
-        count = -1
+        # Method 1. 按照右端点排序
+        intervals.sort(key=lambda x: x[1])
         
         end = intervals[0][1]
+        count = -1
         
-        for a, b in intervals: 
-            if a >= end: # 区间未重叠情况
+        for a, b in intervals:
+            if a >= end:
                 end = b
-                continue
-            else: # 两个区间重叠，需要更新边界为最小值
+            else:
                 count += 1
-                end = min(end, b)
                 
         return count 
+                
+            
+        
+        
+#         # Method 2. 按照左端点排序
+#         intervals.sort(key=lambda x: (x[0], x[1]))
+        
+#         # 第一个边界自动会进入else语句，count+=1变成0，开始计数
+#         count = -1
+        
+#         end = intervals[0][1]
+        
+#         for a, b in intervals: 
+#             if a >= end: # 区间未重叠情况
+#                 end = b
+#                 continue
+#             else: # 两个区间重叠，需要更新边界为最小值
+#                 count += 1
+#                 end = min(end, b)
+                
+#         return count 
