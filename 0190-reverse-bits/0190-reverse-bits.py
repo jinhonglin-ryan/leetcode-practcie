81116d267c5fbcae15c35bc19a86b3c84c1214d7
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-20 23:38:45 +0800

    Time: 17 ms (44.41%), Space: 11.6 MB (21.58%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -5,8 +5,13 @@ class Solution:
         res = 0
         
         for i in range(32):
+            # 获取 n 的最低位
             bit = n & 1
+            
+            # 将 bit 移到对应的位置，并累加到 result
             res = (res << 1) | bit
+            
+            # 右移 n，处理下一位
             n >>= 1
             
         return res 
