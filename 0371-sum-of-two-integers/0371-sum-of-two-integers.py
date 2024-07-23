6c3f5f7f08bc8b3939fbf2fa3b9690c77f831337
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-23 21:53:44 +0800

    Time: 10 ms (79.09%), Space: 11.5 MB (89.04%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -11,20 +11,26 @@ class Solution(object):
         # 重复迭代：确保所有位都被正确加上，包括进位部分，直到没有进位为止。
         
         MAX_INT = 0x7FFFFFFF # 32 位有符号整数的最大值，二进制表示为0和31个1 
-        MASK = 0xFFFFFFFF # 32 位无符号整数的最大值，二进制全是1 
+        MASK = 0xFFFFFFFF # 32 位无符号整数的最大值，二进制全是1
         
-        # 保证a和b都在32位中
+        # 获得a和b的lower 32 bits，把a和b看作32位无符号整数，最后对a的范围进行判断，返回成 有符号整数
         a &= MASK
         b &= MASK
         
+        # 当 b 不为 0 时，表示还有进位需要处理。
         while b != 0:
+            # 计算进位，并将进位左移一位，同时使用 MASK 限制在 32 位内。
             carry = ((a & b) << 1) & MASK
             
+            # 计算不带进位的和
             a = (a ^ b) & MASK
             
             b = carry 
-            
+        
+        
+        # a是一个非负数，直接返回 a。
         if a <= MAX_INT:
             return a
+        # 当 a 超过32位有符号整数的最大值时，溢出，返回对应的负数
         else:
             return ~(a ^ MASK)
