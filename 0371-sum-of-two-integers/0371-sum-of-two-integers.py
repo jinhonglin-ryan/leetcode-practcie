4a51903fd758a85b9e474f7b2f8c3f3a20a9bdd4
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-23 22:09:22 +0800

    Time: 8 ms (85.89%), Space: 11.4 MB (89.04%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -13,21 +13,19 @@ class Solution(object):
         MAX_INT = 0x7FFFFFFF # 32 位有符号整数的最大值，二进制表示为0和31个1 
         MASK = 0xFFFFFFFF # 32 位无符号整数的最大值，二进制全是1
         
-        # 获得a和b的lower 32 bits，把a和b看作32位无符号整数，最后对a的范围进行判断，返回成 有符号整数
-        a &= MASK
-        b &= MASK
-        
+        # 获得 a和b的lower 32 bits，把a和b看作32位无符号整数，最后对a的范围进行判断，返回成 有符号整数
+
         # 当 b 不为 0 时，表示还有进位需要处理。
         while b != 0:
             # 计算进位，并将进位左移一位，同时使用 MASK 限制在 32 位内。
             carry = ((a & b) << 1) & MASK
             
-            # 计算不带进位的和
+            # 计算不带进位的和，确保在32位内
             a = (a ^ b) & MASK
             
             b = carry 
         
-        
+       
         # a是一个非负数，直接返回 a。
         if a <= MAX_INT:
             return a
