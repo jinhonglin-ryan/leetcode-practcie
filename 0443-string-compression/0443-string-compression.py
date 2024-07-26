4b375b4aa68400bc96cde1b50011d37376b8d96c
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-26 11:23:41 +0800

    Time: 32 ms (77.56%), Space: 11.9 MB (32.76%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -4,27 +4,27 @@ class Solution(object):
         :type chars: List[str]
         :rtype: int
         """
-        w = 0  # Index to write the next character or number
-        r = 0  # Index to read the next character
+        w = 0  # 写入的index
+        r = 0  # 读取的index
 
         while r < len(chars):
             char = chars[r]
             cnt = 0
 
-            # Count the occurrences of the current character
+            # 读取连续字符，记录有多少个
             while r < len(chars) and chars[r] == char:
                 r += 1
                 cnt += 1
 
-            # Write the character to the write_index
+            # 写入当前字符
             chars[w] = char
             w += 1
 
-            # If the count is greater than 1, write the count as characters
+            # 如果当前字符cnt > 1，也需要写入
             if cnt > 1:
                 for digit in str(cnt):
                     chars[w] = digit
                     w += 1
 
-        # Return the new length of the array
+        # w是下一个写入的index，即我们目前已经写了多少个（从0开始计算），即最后数组长度
         return w
