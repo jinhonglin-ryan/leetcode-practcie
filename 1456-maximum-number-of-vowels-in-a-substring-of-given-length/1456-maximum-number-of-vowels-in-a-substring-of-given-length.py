a7df13577cddbaa33b9c5a5cb14ab7c646f31567
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-29 17:49:08 +0800

    Time: 208 ms (22.29%), Space: 12.9 MB (88.57%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -6,40 +6,61 @@ class Solution(object):
         :rtype: int
         """
         
-        vowels = set(['a', 'e', 'i', 'o', 'u'])
-        max_cnt = -1
-        
+        # Method 1. 
         left, right = 0, 0
-        
-        curr_sub = ""
-        curr_cnt = 0
-        
-        while right < k:
-            curr_sub += s[right]
-            right += 1
-            
-        for ch in curr_sub:
-            if ch in vowels:
-                curr_cnt += 1
-        
-        max_cnt = max(max_cnt, curr_cnt)
-        
+        ans = 0
+        window_count = 0
+        vowel_set = ('a','e','i','o','u')
+
         while right < len(s):
-            first = curr_sub[0]
-            if first in vowels:
-                curr_cnt -= 1
-            
-            curr_sub = curr_sub[1:]
-            now = s[right]
-            curr_sub += now
+            if s[right] in vowel_set:
+                window_count += 1
+
+            if right - left + 1 >= k:
+                ans = max(ans, window_count)
+                if s[left] in vowel_set:
+                    window_count -= 1
+                left += 1
+
             right += 1
+        return ans
+
+        
+#         # Method 2. 
+#         vowels = set(['a', 'e', 'i', 'o', 'u'])
+#         max_cnt = -1
+        
+#         left, right = 0, 0
+        
+#         curr_sub = ""
+#         curr_cnt = 0
+        
+#         while right < k:
+#             curr_sub += s[right]
+#             right += 1
             
-            if now in vowels:
-                curr_cnt += 1
+#         for ch in curr_sub:
+#             if ch in vowels:
+#                 curr_cnt += 1
+        
+#         max_cnt = max(max_cnt, curr_cnt)
+        
+#         while right < len(s):
+#             first = curr_sub[0]
+#             if first in vowels:
+#                 curr_cnt -= 1
+            
+#             curr_sub = curr_sub[1:]
+#             now = s[right]
+#             curr_sub += now
+#             right += 1
+            
+#             if now in vowels:
+#                 curr_cnt += 1
                 
-            max_cnt = max(max_cnt, curr_cnt)  
+#             max_cnt = max(max_cnt, curr_cnt)  
             
-        return max_cnt
+#         return max_cnt
             
             
             
