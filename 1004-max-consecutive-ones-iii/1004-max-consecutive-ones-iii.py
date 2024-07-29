45f0a0845a9fb82574eddff1f87327ac30609835
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-29 18:17:13 +0800

    Time: 451 ms (36.52%), Space: 11.6 MB (99.71%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -20,6 +20,7 @@ class Solution(object):
             else:
                 curr_zeros += 1
                 
+            # 如果 0 的数量超过了 k，意味着我们变换k次也没有办法获得全部是1，因此需要调整左指针缩小窗口
             while curr_zeros > k:
                 rm = nums[left]
                 if rm == 0:
@@ -28,6 +29,7 @@ class Solution(object):
                     curr_ones -= 1
                 left += 1
                 
+            # 更新最大长度: 如果curr_zeros 没有大于k了，说明我们可以通过至多k次flip把0变成1，因此总共的1就是curr_ones + curr_zeros
             res = max(res, curr_ones + curr_zeros)
             right += 1
             
