859cfe6e7b4859c6ddf9fa80d03fb8307447f353
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-07-30 15:47:16 +0800

    Time: 63 ms (74.42%), Space: 12.7 MB (88.46%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -8,29 +8,22 @@ class Solution(object):
         stack = []
         
         for x in asteroids:
-            if x > 0:
+            if not stack or x > 0:
                 stack.append(x)
                 
-            else:
-                # 如果stack内有东西，且当前的x是向左移动，stack的top是向右移动，会发生碰撞
-                while stack and x < 0 < stack[-1]:
-                    # 如果当前的x跟stack top的质量一样，两个都爆炸，移出top，直接进入下一个asteroids的iteration
-                    if abs(stack[-1]) == abs(x):
-                        stack.pop()
-                        break
-
-                    # 如果x在碰撞后保留，移出stack的top，继续检验while
-                    elif abs(stack[-1]) < abs(x):
-                        stack.pop()
+            
+            else:                
+                while stack and 0 < stack[-1] < -x:
+                    stack.pop()
                     
-                    # 当前x在碰撞中没了
-                    else:
-                        break 
-                        
-                else: 
+                if not stack or stack[-1] < 0:
                     stack.append(x)
                     
+                elif stack[-1] == -x:
+                    stack.pop()
+                    
         return stack 
+            
                 
             
                     
