40ad80767f78154cad4852de59e7492c61a51214
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-08-01 17:06:55 +0800

    Time: 96 ms (20.38%), Space: 15.8 MB (51.42%) - LeetHub

---
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root is None:
+            return TreeNode(val)
+        
+        # 否则递归地插入数据
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        
+        # 返回（未改变的）节点指针
+        return root
