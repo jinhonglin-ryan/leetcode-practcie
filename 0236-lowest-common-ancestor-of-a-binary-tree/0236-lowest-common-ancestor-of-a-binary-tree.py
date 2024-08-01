810fd3cd09a6154d7e0302acbdd70c5d69ba6af7
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-08-01 16:57:39 +0800

    Time: 36 ms (94.35%), Space: 23.6 MB (50.49%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        
+        if root is None or root == p or root == q:
+            return root
+    
+        # 递归在左子树搜索p or q
+        left = self.lowestCommonAncestor(root.left, p, q) if root.left else None
+        # 递归在右子树搜索p or q
+        right = self.lowestCommonAncestor(root.right, p, q) if root.right else None
+
+        # 如果在左右子树中都找到了目标节点，则当前root是LCA
+        if left and right:
+            return root
+
+        # 如果只在左子树找到，则返回左子树的结果
+        if left:
+            return left
+
+        # 如果只在右子树找到，则返回右子树的结果
+        if right:
+            return right
+
+        # 如果两边都没有，则返回None
+        return None
