e1b65758f689dff5ae88ce83f8125f19e8e2b455
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-08-01 16:00:22 +0800

    Time: 701 ms (31.13%), Space: 13.3 MB (19.07%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -5,38 +5,61 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    
-    # Method 1. 前缀和 + 哈希表
-    def __init__(self):
-        self.cnt = 0
-        self.prefixSum = defaultdict(int)
-    
-    def dfs(self, node, currSum, targetSum):
-        if node is None:
-            return
-        
-        # 从根到当前node的总和是currSum
-        currSum += node.val
-        
-        # 我们需要知道在这条路径上有多少条路径等于targetSum, 所以就需要知道currSum - targetSum在哈希表中出现过几次
-        self.cnt += self.prefixSum[currSum - targetSum]
-        
-        # 更新当前路径和的前缀和计数
-        self.prefixSum[currSum] += 1
-        
-        # 递归处理左子树和右子树
-        self.dfs(node.left, currSum, targetSum)
-        self.dfs(node.right, currSum, targetSum)
-        
-        # 回溯，恢复当前路径和的前缀和计数
-        self.prefixSum[currSum] -= 1
-        
     def pathSum(self, root, targetSum):
-        """
-        :type root: TreeNode
-        :type targetSum: int
-        :rtype: int
-        """
-        self.prefixSum[0] = 1 # 哨兵
-        self.dfs(root, 0, targetSum) # 修正初始参数顺序
-        return self.cnt
+        if root is None:
+            return 0
+        
+        # 计算以当前节点为起点的所有路径和等于targetSum的路径数
+        def dfs(node, targetSum):
+            if node is None:
+                return 0
+            current = node.val
+            totalPaths = 0
+            if current == targetSum:
+                totalPaths += 1
+            totalPaths += dfs(node.left, targetSum - current)
+            totalPaths += dfs(node.right, targetSum - current)
+            return totalPaths
+        
+        # 计算当前节点、左子树和右子树中的路径数
+        pathsFromRoot = dfs(root, targetSum)
+        pathsOnLeft = self.pathSum(root.left, targetSum)
+        pathsOnRight = self.pathSum(root.right, targetSum)
+        
+        return pathsFromRoot + pathsOnLeft + pathsOnRight
+        
+    
+#     # Method 1. 前缀和 + 哈希表
+#     def __init__(self):
+#         self.cnt = 0
+#         self.prefixSum = defaultdict(int)
+    
+#     def dfs(self, node, currSum, targetSum):
+#         if node is None:
+#             return
+        
+#         # 从根到当前node的总和是currSum
+#         currSum += node.val
+        
+#         # 我们需要知道在这条路径上有多少条路径等于targetSum, 所以就需要知道currSum - targetSum在哈希表中出现过几次
+#         self.cnt += self.prefixSum[currSum - targetSum]
+        
+#         # 更新当前路径和的前缀和计数
+#         self.prefixSum[currSum] += 1
+        
+#         # 递归处理左子树和右子树
+#         self.dfs(node.left, currSum, targetSum)
+#         self.dfs(node.right, currSum, targetSum)
+        
+#         # 回溯，恢复当前路径和的前缀和计数
+#         self.prefixSum[currSum] -= 1
+        
+#     def pathSum(self, root, targetSum):
+#         """
+#         :type root: TreeNode
+#         :type targetSum: int
+#         :rtype: int
+#         """
+#         self.prefixSum[0] = 1 # 哨兵
+#         self.dfs(root, 0, targetSum)
+#         return self.cnt
