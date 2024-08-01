3d84ffdf69d915641325c9e428d5a6f3a0c379cc
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-08-01 12:05:11 +0800

    Time: 41 ms (98.07%), Space: 27.1 MB (95.34%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -10,10 +10,12 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        
+        # 终止条件1：如果当前节点是空节点，返回0
         if root is None:
             return 0
         
+        # 终止条件2：如果当前节点的左子树和右子树是一个满二叉树，可以利用公式把当前节点+左子树和右子树的所有node count算出来
+        # 如果当前节点的左子树和右子树是一个满二叉树，则满足当前节点一直往左走的深度 和当前节点一直往右走的深度一样 
         left = root
         right = root
         
@@ -31,6 +33,7 @@ class Solution(object):
         if left_height == right_height:
             return (2 ** left_height) - 1
         
+        # 单层递归，如果当前节点的左右子树不是满二叉树，则递归去算左子树的node count 和右子树的node count，然后加上当前节点+1返回 
         leftNum = self.countNodes(root.left)
         rightNum = self.countNodes(root.right)
         
