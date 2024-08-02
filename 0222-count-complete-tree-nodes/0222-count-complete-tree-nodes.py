0d7608ad4e8c664a2e3b63a45de0d3e6b4a08c0d
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-08-02 20:23:43 +0800

    Time: 66 ms (24.43%), Space: 27.5 MB (22.24%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -5,30 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    def __init__(self):
+        self.cnt = 0
     def countNodes(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        
         if root is None:
             return 0
-        
-        left = root.left
-        right = root.right
-        
-        lh = 1
-        rh = 1
-        
-        while left:
-            left = left.left
-            lh += 1
+        else :
+            self.cnt += 1
             
-        while right:
-            right = right.right
-            rh += 1
-            
-        if rh == lh:
-            return (2 ** lh) - 1
+        if root.left:
+            self.countNodes(root.left)
         
-        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+        if root.right:
+            self.countNodes(root.right)
+            
+        return self.cnt 
+        
+        
