4a9761a4c28c56d3bf20a1284dce76f3d06bb0a8
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-08-03 17:11:36 +0800

    Time: 37 ms (21.18%), Space: 19.4 MB (11.18%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.py
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.py
@@ -19,10 +19,11 @@ class Solution(object):
         if root.val < low:
             return self.trimBST(root.right, low, high)
         
-        if root.val > high:
+        elif root.val > high:
             return self.trimBST(root.left, low, high)
         
-        root.left = self.trimBST(root.left, low, high)
+        root.left = self.trimBST(root.left,low, high)
+        
         root.right = self.trimBST(root.right, low, high)
         
-        return root 
+        return root
