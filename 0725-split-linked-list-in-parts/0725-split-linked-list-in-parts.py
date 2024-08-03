73bec7088dc88c0d1c3ebbb928f1f859e4fab137
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-08-04 01:42:58 +0800

    Time: 24 ms (21.28%), Space: 12.8 MB (5.32%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
@@ -11,15 +11,16 @@ class Solution(object):
         :rtype: List[ListNode]
         """
         
-        res = []
-        cnt = 0
+        length = 0
         curr = head
+        
         while curr:
             curr = curr.next
-            cnt += 1
-        
-        num = cnt / k
-        rem = cnt % k
+            length += 1
+            
+        num = length / k
+        rem = length % k
+        res = []
         
         for i in range(k):
             dummy = ListNode()
@@ -30,21 +31,17 @@ class Solution(object):
                 each.next = node
                 each = each.next
                 head = head.next
-            
+                
             if rem and head:
                 nodex = ListNode(head.val)
                 each.next = nodex
                 head = head.next
                 rem -= 1
-            
+                
             res.append(dummy.next)
-        
-        return res 
             
-    
-                    
+        return res 
                 
                 
-                
-                                
-    
+            
+        
