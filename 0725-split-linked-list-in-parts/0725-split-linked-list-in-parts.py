4311b0dd10f9f29325497b3973ba90770b8433fc
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-08-04 01:45:38 +0800

    Time: 25 ms (17.02%), Space: 12.1 MB (28.72%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
@@ -11,37 +11,35 @@ class Solution(object):
         :rtype: List[ListNode]
         """
         
-        length = 0
+        res = []
+        cnt = 0
         curr = head
-        
         while curr:
             curr = curr.next
-            length += 1
-            
-        num = length / k
-        rem = length % k
-        res = []
+            cnt += 1
+        
+        num = cnt / k
+        rem = cnt % k
         
         for i in range(k):
             dummy = ListNode()
             each = dummy
-            
-            for j in range(num):
-                node = ListNode(head.val)
-                each.next = node
+            for j in range(num + (rem > 0)):  # 当 rem > 0 时，每部分多一个节点
+                each.next = head
                 each = each.next
-                head = head.next
-                
-            if rem and head:
-                nodex = ListNode(head.val)
-                each.next = nodex
-                head = head.next
+                if head:
+                    head = head.next
+            if rem > 0:
                 rem -= 1
-                
+            each.next = None
             res.append(dummy.next)
-            
+
         return res 
-                
-                
             
-        
+    
+                    
+                
+                
+                
+                                
+    
