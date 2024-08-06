6e81bae5b6c00241a8d034c974254f1877732998
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-08-07 04:38:12 +0800

    Time: 24 ms (95.31%), Space: 12.7 MB (80.34%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -5,41 +5,31 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-   
-        
-    
-    # Method 1. 前缀和 + 哈希表
     def __init__(self):
         self.cnt = 0
-        self.prefixSum = defaultdict(int)
-    
-    def dfs(self, node, currSum, targetSum):
+        self.map = defaultdict(int)
+        
+    def dfs(self, node, curr, target):
         if node is None:
             return
         
-        # 从根到当前node的总和是currSum
-        currSum += node.val
+        curr += node.val
+        self.cnt += self.map[curr - target]
+        self.map[curr] += 1
+        self.dfs(node.right, curr, target)
+        self.dfs(node.left, curr, target)
+        self.map[curr] -= 1
         
-        # 我们需要知道在这条路径上有多少条路径等于targetSum, 所以就需要知道currSum - targetSum在哈希表中出现过几次
-        self.cnt += self.prefixSum[currSum - targetSum]
         
-        # 更新当前路径和的前缀和计数
-        self.prefixSum[currSum] += 1
-        
-        # 递归处理左子树和右子树
-        self.dfs(node.left, currSum, targetSum)
-        self.dfs(node.right, currSum, targetSum)
-        
-        # 回溯，恢复当前路径和的前缀和计数
-        self.prefixSum[currSum] -= 1
-        
+    
     def pathSum(self, root, targetSum):
         """
         :type root: TreeNode
         :type targetSum: int
         :rtype: int
         """
-        self.prefixSum[0] = 1 # 哨兵
+        
+        self.map[0] = 1
         self.dfs(root, 0, targetSum)
         return self.cnt
     
