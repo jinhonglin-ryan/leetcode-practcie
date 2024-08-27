16485d4b2978bb2fafcf4bf26e433d0e9d7515c3
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-08-28 05:15:28 +0800

    Time: 28 ms (71.25%), Space: 11.9 MB (58.87%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -9,62 +9,35 @@ class Solution(object):
         # 单调栈
         # 在nums2找一个数的右边的第一个最大的数
         # 保持从栈头到栈尾递增的栈，遍历nums2，加入索引
-#         ans = [-1 for _ in range(len(nums1))]
+        ans = [-1 for _ in range(len(nums1))]
     
-#         umap = defaultdict(int)
-#         # 建立数组1，数字到index的dictionary
-#         for i in range(len(nums1)):
-#             umap[nums1[i]] = i
-            
-#         stack = [0]
-#         for i in range(1, len(nums2)):
-            
-#             # 如果当前数字小于等于当前栈头，加入单调栈，维持递增
-#             if nums2[i] <= nums2[stack[-1]]:
-#                 stack.append(i)
-            
-#             # 如果当前数字大于栈头，需要移除栈中比当前数字小的那些数
-#             else:
-#                 while stack and nums2[i] > nums2[stack[-1]]:
-#                     # 如果当前栈头在umap中，说明当前nums[i]为当前栈头对应的index在nums2中的下一个大大数，需要更新到ans
-#                     if stack[-1] in umap:
-#                         num = nums2[i]
-#                         index = umap[stack[-1]]
-#                         ans[index] = num
-#                     stack.pop()
-                
-#                 # 循环完成后，nums[i] < nums[stack[-1]]，所以需要加入stack
-#                 stack.append(i)
-                
-#         return ans
-
-        stack = []
-        result = [-1] * len(nums1)
-        if len(nums1) == 0:
-            return result
-        
-        umap = {}  # A dictionary to map elements of nums1 to their indices
+        umap = {}
+        # 建立数组1，数字到index的dictionary
         for i in range(len(nums1)):
             umap[nums1[i]] = i
-        
-        stack.append(0)
+            
+        stack = [0]
         for i in range(1, len(nums2)):
-            if nums2[i] < nums2[stack[-1]]:
+            
+            # 如果当前数字小于等于当前栈头，加入单调栈，维持递增
+            if nums2[i] <= nums2[stack[-1]]:
                 stack.append(i)
-            elif nums2[i] == nums2[stack[-1]]:
-                stack.append(i)
+            
+            # 如果当前数字大于栈头，需要移除栈中比当前数字小的那些数
             else:
                 while stack and nums2[i] > nums2[stack[-1]]:
+                    # 如果当前栈头在umap中，说明当前nums[i]为当前栈头对应的index在nums2中的下一个大大数，需要更新到ans
                     if nums2[stack[-1]] in umap:
+                        num = nums2[i]
                         index = umap[nums2[stack[-1]]]
-                        result[index] = nums2[i]
+                        ans[index] = num
                     stack.pop()
+                
+                # 循环完成后，nums[i] < nums[stack[-1]]，所以需要加入stack
                 stack.append(i)
-        
-        return result
+                
+        return ans
                 
                     
-            
-            
-        
+     
         
