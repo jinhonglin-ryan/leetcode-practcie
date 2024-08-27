730d4d0d5976fde4a938c2f8dd17ab8aa1746c5c
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-08-28 05:11:26 +0800

    Time: 35 ms (48.99%), Space: 11.8 MB (58.87%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,70 @@
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        
+        # 单调栈
+        # 在nums2找一个数的右边的第一个最大的数
+        # 保持从栈头到栈尾递增的栈，遍历nums2，加入索引
+#         ans = [-1 for _ in range(len(nums1))]
+    
+#         umap = defaultdict(int)
+#         # 建立数组1，数字到index的dictionary
+#         for i in range(len(nums1)):
+#             umap[nums1[i]] = i
+            
+#         stack = [0]
+#         for i in range(1, len(nums2)):
+            
+#             # 如果当前数字小于等于当前栈头，加入单调栈，维持递增
+#             if nums2[i] <= nums2[stack[-1]]:
+#                 stack.append(i)
+            
+#             # 如果当前数字大于栈头，需要移除栈中比当前数字小的那些数
+#             else:
+#                 while stack and nums2[i] > nums2[stack[-1]]:
+#                     # 如果当前栈头在umap中，说明当前nums[i]为当前栈头对应的index在nums2中的下一个大大数，需要更新到ans
+#                     if stack[-1] in umap:
+#                         num = nums2[i]
+#                         index = umap[stack[-1]]
+#                         ans[index] = num
+#                     stack.pop()
+                
+#                 # 循环完成后，nums[i] < nums[stack[-1]]，所以需要加入stack
+#                 stack.append(i)
+                
+#         return ans
+
+        stack = []
+        result = [-1] * len(nums1)
+        if len(nums1) == 0:
+            return result
+        
+        umap = {}  # A dictionary to map elements of nums1 to their indices
+        for i in range(len(nums1)):
+            umap[nums1[i]] = i
+        
+        stack.append(0)
+        for i in range(1, len(nums2)):
+            if nums2[i] < nums2[stack[-1]]:
+                stack.append(i)
+            elif nums2[i] == nums2[stack[-1]]:
+                stack.append(i)
+            else:
+                while stack and nums2[i] > nums2[stack[-1]]:
+                    if nums2[stack[-1]] in umap:
+                        index = umap[nums2[stack[-1]]]
+                        result[index] = nums2[i]
+                    stack.pop()
+                stack.append(i)
+        
+        return result
+                
+                    
+            
+            
+        
+        
