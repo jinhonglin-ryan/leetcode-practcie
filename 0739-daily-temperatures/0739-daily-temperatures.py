e5718284a970c152d32f231c1ee803439b60fa06
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-08-28 04:53:36 +0800

    Time: 1011 ms (66.61%), Space: 25.1 MB (79.58%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -5,12 +5,11 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        # 用一个栈来保存未找到比它们更高温度的那些天的索引
-        # 栈中的索引对应的实际温度是单调递减的，这样可以确保每次遇到一个更高温度时，可以一次性处理掉栈中所有较低温度的天数。
-        # 单调递减，为什么？如果当前温度 temperatures[i] 小于或等于栈顶索引对应的温度 temperatures[stack[-1]]，我们直接将当前索引 i 压入栈中。这样保持了栈中温度的单调递减性。
-        # 当我们遇到一个更高的温度 temperatures[i] 时，我们需要处理栈中所有比当前温度低的温度。这时，我们会弹出栈顶元素，并更新这些温度对应的等待天数。
+        # Method 1 用单调栈做
+        # 本质上是找到当前元素右边的最大元素
+        # 求一个元素右边第一个更大元素，单调栈就是递增的：递增为从栈头到栈底是递增的
+        # 栈中存索引
         
-
         n = len(temperatures)
         ans = [0 for _ in range(n)]
         stack = []
@@ -20,5 +19,21 @@ class Solution(object):
                 index = stack.pop()
                 ans[index] = i - index
             stack.append(i)
+        
         return ans
+    
+        # Method 2 暴力解法（超时）
+#         n = len(temperatures)
+#         count = {i: 0 for i in range(n)}
+#         ans = [0] * n
+
+#         for i in range(n):
+#             for j in range(i + 1, n):
+#                 if temperatures[j] > temperatures[i]:
+#                     count[i] = j - i
+#                     break
+#         for i in range(n):
+#             ans[i] = count[i]
+
+#         return ans
                 
