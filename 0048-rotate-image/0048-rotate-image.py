970bfb04a3e6d5aa8f879135f2582b0ea58c81ee
Ryan Lin <123346659+jinhonglin-ryan@users.noreply.github.com>
2024-10-08 23:40:00 -0400

    Time: 46 ms (9.01%), Space: 16.5 MB (53.80%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,34 +1,16 @@
-class Solution(object):
-    def rotate(self, matrix):
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
         """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
+        Do not return anything, modify matrix in-place instead.
         """
-        
-        # 旋转90度被拆成
-        # 1. 沿着对角线翻转
-        # 2. 沿着中轴翻转 
-        
-        
+
         n = len(matrix)
-        
-        # 沿对角线翻转
+
+        for i in range(n // 2):
+            for j in range(n):
+                matrix[i][j], matrix[n - i - 1][j] = matrix[n - i - 1][j], matrix[i][j]
+
+
         for i in range(n):
             for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-                
-                
-        # 沿中轴翻转
-        for i in range(n):
-            j = 0
-            k = n - 1
-            while j < k:
-                # 交换 matrix[i][j] 和 matrix[i][k]
-                matrix[i][j], matrix[i][k] = matrix[i][k], matrix[i][j]
-                j += 1
-                k -= 1
-                
-        return matrix
-        
-                
-        
